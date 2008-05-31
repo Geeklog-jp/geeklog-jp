b274a93ed32909b053aa78be431bb7907909034f
dengenxp <dengenxp@localhost>
2008-05-31 02:16:09 +0000

    ??CVS???????

--- language/polish_utf-8.php
+++ language/polish_utf-8.php
@@ -1,7 +1,7 @@
 <?php
 
 ###############################################################################
-# polish.php
+# polish_utf-8.php
 # This is the Polish language page for GeekLog!
 # Special thanks to Robert Stadnik geeklog@geeklog.now.pl for his work on this project
 #
@@ -24,7 +24,7 @@
 #
 ###############################################################################
 
-$LANG_CHARSET = 'UTF-8';
+$LANG_CHARSET = 'utf-8';
 
 ###############################################################################
 # Array Format:
@@ -154,23 +154,23 @@ $LANG01 = array(
     111 => 'Wersja PDF',
     112 => 'Zarejestrowani Użytkownicy',
     113 => 'Dokumentacja',
-    114 => 'TRACKBACKS',
-    115 => 'No new trackback comments',
-    116 => 'Trackback',
-    117 => 'Directory',
-    118 => 'Please continue reading on the next page:',
-    119 => "Lost your <a href=\"{$_CONF['site_url']}/users.php?mode=getpassword\" rel=\"nofollow\">password</a>?",
-    120 => 'Permanent link to this comment',
-    121 => 'Comments (%d)',
-    122 => 'Trackbacks (%d)',
-    123 => 'All HTML is allowed',
-    124 => 'Click to delete all checked items',
-    125 => 'Are you sure you want to Delete all checked items?',
-    126 => 'Select or de-select all items',
-    127 => 'Permalink',
-    128 => 'Login with OpenID:',
-    129 => 'Configuration',
-    130 => 'Webservices'
+    114 => 'POWIADOMIENIA ZWROTNE',
+    115 => 'Brak nowych powiadomień zwrotnych dla komentarzy',
+    116 => 'Powiadomienia zwrotne',
+    117 => 'Katalog',
+    118 => 'Dalszy ciąg na kolejnej stronie:',
+    119 => "Nie pamiętasz <a href=\"{$_CONF['site_url']}/users.php?mode=getpassword\" rel=\"nofollow\">hasła</a>?",
+    120 => 'Stały odnośnik do tego komentarza',
+    121 => 'Komentarze (%d)',
+    122 => 'Powiadomienia zwrotne (%d)',
+    123 => 'Wszystkie znaczniki HTML są dozwolone',
+    124 => 'Kliknij aby usunąć wszystkie zaznaczone pozycje',
+    125 => 'Czy aby na pewno chcesz Usunąć wszystkie zaznaczone pozycje?',
+    126 => 'Zaznacz lub odznacz wszystkie pozycje',
+    127 => 'Stały odnośnik',
+    128 => 'Zaloguj z OpenID:',
+    129 => 'Konfiguracja',
+    130 => 'Usługi Web-owe'
 );
 
 ###############################################################################
@@ -222,7 +222,7 @@ $LANG04 = array(
     10 => 'Ostatnie 10 komentarzy użytkownika',
     11 => 'Brak Komentarzy Użytkownika',
     12 => 'Preferencje Użytkownika',
-    13 => 'Email Nightly Digest',
+    13 => 'Dzienne Zestawienie Artykułów',
     14 => 'Hasło jest generowane automatycznie. Zalecana jest szybka zmiana hasła. Aby zmienić hasło, należy się zalogować a następnie kliknąć na Konto Info w Menu Użytkownika.',
     15 => "Twoje {$_CONF['site_name']} konto zostało założone. Zaloguj się wykorzystując informacje poniżej. Proszę zachować tego maila w bezpiecznym miejscu.",
     16 => 'Informacje Dotyczące Twojego Konta',
@@ -234,7 +234,7 @@ $LANG04 = array(
     22 => "Zarejestruj się w serwisie {$_CONF['site_name']}!",
     23 => "Konto w serwisie {$_CONF['site_name']} pozwoli ci zamieszczać komentarze i inne pozycje w twoim imieniu. Brak konta umożliwia tylko zamieszczanie jako anonim. Twój adres email <b><i>nigdy</i></b> nie będzie widoczny publicznie.",
     24 => 'Twoje hasło zostanie wysłane pod podany adres email.',
-    25 => 'Zapomniałeś/aś hasło?',
+    25 => 'Nie pamiętasz hasła?',
     26 => 'Wpisz <em>albo</em> swój login <em>albo</em> adres email podany podczas rejestracji i kliknij Prześlij Hasło. Instrukcje jak ustawić nowe hasło zostaną wysłane do Ciebie mailem.',
     27 => 'Zarejestruj Się!',
     28 => 'Prześlij Hasło',
@@ -317,53 +317,53 @@ $LANG04 = array(
     105 => 'Pokaż w bloku Who\'s Online',
     106 => 'Lokalizacja',
     107 => 'Pokaż w swoim publicznym profilu',
-    108 => 'Confirm new password',
-    109 => 'Enter the New password again here',
-    110 => 'Current Password',
-    111 => 'Please enter your Current password',
+    108 => 'Potwierdź nowe hasło',
+    109 => 'Wpisz tutaj ponownie Nowe hasło',
+    110 => 'Obecne Hasło',
+    111 => 'Proszę wpisać swoje Obecne hasło',
     112 => 'You have exceeded the number of allowed login attempts.  Please try again later.',
-    113 => 'Login Attempt Failed',
-    114 => 'Account Disabled',
-    115 => 'Your account has been disabled, you may not login. Please contact an Administrator.',
-    116 => 'Account Awaiting Activation',
-    117 => 'Your account is currently awaiting activation by an administrator. You will not be able to login until your account has been approved.',
+    113 => 'Nieudana Próba Zalogowania',
+    114 => 'Konto WYłączone',
+    115 => 'Twoje konto zostało wyłączone, nie można się zalogować. Proszę skontaktować się z Administratorem.',
+    116 => 'Konto Czeka Na Aktywację',
+    117 => 'Twoje konto czeka obecnie na aktywację przez administratora. You will not be able to login until your account has been approved.',
     118 => "Your {$_CONF['site_name']} account has now been activated by an administrator. You may now login to the site at the url below using your username (<username>) and password as previously emailed to you.",
-    119 => 'If you have forgotten your password, you may request a new one at this url:',
-    120 => 'Account Activated',
+    119 => 'Jeśli nie pamiętasz swojego hasła, kliknij na ten odnośnik aby otrzymać nowe:',
+    120 => 'Konto Zostało Aktywowane',
     121 => 'Service',
-    122 => 'Sorry, new user registration is disabled',
-    123 => "Are you a <a href=\"{$_CONF['site_url']}/users.php?mode=new\" rel=\"nofollow\">new user</a>?",
-    124 => 'Confirm Email',
-    125 => 'You have to enter the same email address in both fields!',
+    122 => 'Przepraszamy, rejestracja nowych użytkowników jest wyłączona',
+    123 => "Czy jesteś <a href=\"{$_CONF['site_url']}/users.php?mode=new\" rel=\"nofollow\">nowym użytkownikiem</a>?",
+    124 => 'Potwierdzenie Adresu Email',
+    125 => 'W obu polach należy wpisać taki sam adres email!',
     126 => 'Please repeat for confirmation',
-    127 => 'To change any of these settings, you will have to enter your current password.',
-    128 => 'Your Name',
-    129 => 'Password &amp; Email',
-    130 => 'About You',
+    127 => 'Aby zmienić którekolwiek z tych ustawień, musisz podać swoje bieżące hasło.',
+    128 => 'Twoje Imię',
+    129 => 'Hasło &amp; Email',
+    130 => 'O Sobie',
     131 => 'Daily Digest Options',
     132 => 'Daily Digest Feature',
-    133 => 'Comment Display',
-    134 => 'Comment Options',
-    135 => '<li>Default mode for how comments will be displayed</li><li>Default order to display comments</li><li>Set maximum number of comments to show - default is 100</li>',
-    136 => 'Exclude Topics and Authors',
-    137 => 'Filter Story Content',
-    138 => 'Misc Settings',
-    139 => 'Layout and Language',
+    133 => 'Wyświetlanie Komentarzy',
+    134 => 'Opcje Komentarzy',
+    135 => '<li>Domyślny tryb wyświetlania komentarzy</li><li>Domyślne sortowanie komentarzy</li><li>Liczba wyświetlanych komentarzy - domyślnie jest 100</li>',
+    136 => 'Nie Pokazuj Tematów i Autorów',
+    137 => 'Filtruj Zawartość Artykułów',
+    138 => 'Ustawienia Różne',
+    139 => 'Layout i Język',
     140 => '<li>No Topic Icons if checked will not display the story topic icons</li><li>No boxes if checked will only show the Admin Menu, User Menu and Topics<li>Set the maximum number of stories to show per page</li><li>Set your theme and perferred date format</li>',
-    141 => 'Privacy Settings',
+    141 => 'Ustawienia Prywatności',
     142 => 'The default setting is to allow users & admins to email fellow site members and show your status as online. Un-check these options to protect your privacy.',
-    143 => 'Filter Block Content',
-    144 => 'Show & hide boxes',
-    145 => 'Your Public Profile',
-    146 => 'Password and email',
+    143 => 'Filtruj Zawartość Bloków',
+    144 => 'Pokaż & ukryj pola',
+    145 => 'Twój Profil Publiczny',
+    146 => 'Hasło i email',
     147 => 'Edit your account password, email and autologin feature. You will need to enter the same password or email address twice as a confirmation.',
-    148 => 'User Information',
+    148 => 'Informacje o Użytkowniku',
     149 => 'Modify your user information that will be shown to other users.<li>The signature will be added to any comments or forum posts you made</li><li>The BIO is a brief summary of yourself to share</li><li>Share your PGP Key</li>',
-    150 => 'Warning: Javascript recommended for enhanced functionality',
-    156 => 'Delete Account',
+    150 => 'Uwaga: Javascript jest rekomendowany aby korzystać z zaawansowanych funkcji',
+    156 => 'Usuń Konto',
     157 => 'Delete Option',
-    158 => 'Time Zone',
-    159 => 'This email was generated automatically. Please do not reply to this email.'
+    158 => 'Strefa Czasowa',
+    159 => 'Ta wiadomość została wygenerowana automatycznie. Proszę nie odpowiadać.'
 );
 
 ###############################################################################
@@ -371,12 +371,12 @@ $LANG04 = array(
 # Array index key matches preference div id
 
 $LANG_MYACCOUNT = array(
-    'pe_preview' => 'Preview',
-    'pe_namepass' => 'Username &amp; Password',
-    'pe_userinfo' => 'About You',
-    'pe_layout' => 'Layout &amp; Language',
-    'pe_content' => 'Content',
-    'pe_privacy' => 'Privacy'
+    'pe_preview' => 'Podgląd',
+    'pe_namepass' => 'Użytkownik &amp; Hasło',
+    'pe_userinfo' => 'O Sobie',
+    'pe_layout' => 'Layout &amp; Język',
+    'pe_content' => 'Zawartość',
+    'pe_privacy' => 'Prywtność'
 );
 
 ###############################################################################
@@ -495,10 +495,10 @@ $LANG09 = array(
     55 => 'fraza',
     56 => 'AND',
     57 => 'OR',
-    58 => 'More results &gt;&gt;',
-    59 => 'Results',
-    60 => 'per page',
-    61 => 'Refine search'
+    58 => 'WIęcej wyników &gt;&gt;',
+    59 => 'Wyniki',
+    60 => 'na stronę',
+    61 => 'Zmień zapytanie'
 );
 
 ###############################################################################
@@ -529,12 +529,12 @@ $LANG10 = array(
     22 => '10 Najczęściej Emaliowanych Artykułów',
     23 => 'Emaile',
     24 => 'Wygląda na to, że nikt nie przesłał swoim znajomym żadnego artykułu',
-    25 => 'Top Ten Trackback Commented Stories',
-    26 => 'No trackback comments found.',
-    27 => 'Number of active users',
+    25 => '10 Najczęściej Komentowanych Zwrotnie Artykułów',
+    26 => 'Brak komentarzy zwrotnych.',
+    27 => 'Liczba aktywnych użytkowników',
     28 => '',
     29 => '',
-    30 => 'Hits',
+    30 => 'Odsłon',
     31 => ''
 );
 
@@ -606,8 +606,8 @@ $LANG12 = array(
     51 => 'Lokalizacja',
     52 => 'Kasuj',
     53 => 'Załóż Konto',
-    54 => 'Story Introduction',
-    55 => 'Story Body'
+    54 => 'Artykuł Wstęp',
+    55 => 'Artykuł Część Główna'
 );
 
 ###############################################################################
@@ -691,13 +691,13 @@ $LANG21 = array(
     58 => 'Przesuń Blok w Górę',
     59 => 'Przesuń blok na prawo',
     60 => 'Przesuń blok w lewo',
-    61 => 'No Title',
-    62 => 'Article Limit',
-    63 => 'Bad Block Title',
-    64 => 'Your Title must not be empty and cannot contain HTML!',
-    65 => 'Order',
-    66 => 'Autotags',
-    67 => 'Check to allow autotags',
+    61 => 'Brak Tytułu',
+    62 => 'Limit Artykułów',
+    63 => 'Nieprawidłówy Tytuł Bloku',
+    64 => 'Pole Tytuł nie może być puste i nie może zawierać znaczników HTML!',
+    65 => 'Sorotwanie',
+    66 => 'Autotagi',
+    67 => 'Zaznacz aby włączyć autotagi',
     68 => 'The feed for this portal block is too long to display. Please set a maximum number of articles to import for the block in the block setup screen, or a global maximum in Geeklog Configuration.'
 );
 
@@ -729,10 +729,10 @@ $LANG24 = array(
     22 => 'Lista Artykułów',
     23 => 'Aby zmodyfikować lub wykasować artykuł, kliknij na numer danego artykułu poniżej. Aby przeglądnąć artykuł kliknij na tytuł danego artykułu. Aby wpisać nowy artykuł kliknij na Nowy Artykuł powyżej.',
     24 => 'The ID you chose for this story is already in use. Please use another ID.',
-    25 => 'Error when saving story',
+    25 => 'Błąd podczas zapisywania artykułu',
     26 => 'Podgląd Artykułu',
-    27 => 'If you use [unscaledX] instead of [imageX], the image will be inserted at its original dimensions.',
-    28 => '<p><b>PREVIEWING</b>: Previewing a story with images attached is best done by saving the article as a draft INSTEAD OF hitting the preview button.  Use the preview button only when images are not attached.',
+    27 => 'Jeśli używasz [unscaledX] zamiast [imageX], zdjęcie zostanie zamieszczone w oryginalnym rozmiarze.',
+    28 => '<p><b>PODGLĄD</b>: Podgląd artykułu z załączonymi zdjęciami działa najlepiej po uprzednim zapisaniu artykułu jako kopia robocza ZAMIAST klikaniu na przycisk podglądu.  Przycisk podglądu najlepiej używać gdy nie załączamy zdjęć.',
     29 => 'Trackbacks',
     30 => 'Błąd Uploadu Pliku',
     31 => 'Proszę wpisać Tytuł i Wstęp',
@@ -771,28 +771,28 @@ $LANG24 = array(
     64 => '',
     65 => '',
     66 => '',
-    67 => 'Expand the Content Edit Area size',
-    68 => 'Reduce the Content Edit Area size',
-    69 => 'Publish Story Date',
-    70 => 'Toolbar Selection',
-    71 => 'Basic Toolbar',
+    67 => 'Powiększ Edytor',
+    68 => 'Zmniejsz Edytor',
+    69 => 'Data Publikacji Artykułu',
+    70 => 'Wybór Paska Narzędzi',
+    71 => 'Pasek Podstawowy',
     72 => 'Common Toolbar',
-    73 => 'Advanced Toolbar',
-    74 => 'Advanced II Toolbar',
-    75 => 'Full Featured',
-    76 => 'Publish Options',
-    77 => 'Javascript needs to be enabled for Advanced Editor. Option can be disabled in the main site config.php',
-    78 => 'Click <a href="%s/story.php?mode=edit&amp;sid=%s&amp;editopt=default">here</a> to use default editor',
-    79 => 'Preview',
-    80 => 'Editor',
-    81 => 'Publish Options',
-    82 => 'Images',
-    83 => 'Archive Options',
-    84 => 'Permissions',
-    85 => 'Show All',
-    86 => 'Advanced Editor',
-    87 => 'Story Stats',
-    88 => 'Wiki-style format'
+    73 => 'Pasek Zaawansowany',
+    74 => 'Pasek Zaawansowany II',
+    75 => 'Pełna Opcja',
+    76 => 'Opcje Publikowania',
+    77 => 'Edytor Zaawansowany wymaga włączenia Javascript-u. Opcja może zostać wyłączona w głównym pliku config.php',
+    78 => 'Kliknij <a href="%s/story.php?mode=edit&amp;sid=%s&amp;editopt=default">tutaj</a> aby włączyć edytor domyślny',
+    79 => 'Podgląd',
+    80 => 'Edytor',
+    81 => 'Opcje Publikacji',
+    82 => 'Zdjęcia',
+    83 => 'Opcje Archiwizacji',
+    84 => 'Dostęp',
+    85 => 'Pokaż Wszystko',
+    86 => 'Edytor Zaawansowany',
+    87 => 'Artykuł - Statystyki',
+    88 => 'Format w stylu Wiki'
 );
 
 ###############################################################################
@@ -825,9 +825,9 @@ $LANG27 = array(
     24 => '(*)',
     25 => 'Sekcja Archiwalna',
     26 => 'domyślna sekcja dla archiwizowanych artykułów. Dozwolona jest tylko jedna sekcja.',
-    27 => 'Or Upload Topic Icon',
-    28 => 'Maximum',
-    29 => 'File Upload Errors'
+    27 => 'Lub Wgraj Ikonę Sekcji',
+    28 => 'Max',
+    29 => 'Błąd podczas przesyłania pliku'
 );
 
 ###############################################################################
