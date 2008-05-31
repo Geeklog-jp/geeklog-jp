ef586f2c7b6ed52bcc8ede432a0a4feae1f20371
dengenxp <devnull@localhost>
2008-05-31 02:16:09 +0000

    本家CVSの変更をマージ

--- Geeklog-1.x/public_html/admin/install/language/polish.php
+++ Geeklog-1.x/public_html/admin/install/language/polish.php
@@ -4,9 +4,9 @@
 // +---------------------------------------------------------------------------+
 // | Geeklog 1.5                                                               |
 // +---------------------------------------------------------------------------+
-// | german.php                                                                |
+// | polish.php                                                                |
 // |                                                                           |
-// | German language file for the Geeklog installation script                  |
+// |  Polish language file for the Geeklog installation script                 |
 // +---------------------------------------------------------------------------+
 // | Copyright (C) 2000-2008 by the following authors:                         |
 // |                                                                           |
@@ -34,15 +34,15 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 //
-// $Id: german.php,v 1.9 2008/05/28 18:46:52 dhaun Exp $
+// $Id: polish.php,v 1.1 2008/05/28 18:45:15 dhaun Exp $
 
 // +---------------------------------------------------------------------------+
 
-$LANG_CHARSET = 'iso-8859-15';
+$LANG_CHARSET = 'ISO-8859-2';
 
 // +---------------------------------------------------------------------------+
 // | Array Format:                                                             |
-// | $LANG_NAME[XX]:  $LANG - variable name                                    |
+// | $LANG_NAME[XX]: $LANG - variable name                                    |
 // |                 NAME  - where array is used                               |
 // |                 XX    - phrase id number                                  |
 // +---------------------------------------------------------------------------+
@@ -51,75 +51,75 @@ $LANG_CHARSET = 'iso-8859-15';
 // install.php
 
 $LANG_INSTALL = array(
-    0 => 'Geeklog - Zuverlässigkeit eingebaut',
-    1 => 'Hilfe zur Installation',
-    2 => 'Zuverlässigkeit eingebaut',
-    3 => 'Geeklog-Installation',
-    4 => 'PHP 4.1.0 benötigt',
-    5 => 'Sorry, Geeklog benötigt mindestens PHP 4.1.0 (Du hast Version ',
-    6 => '). Bitte <a href="http://www.php.net/downloads.php">aktualisiere Deine PHP-Installation</a> oder bitte Deinen Hosting-Provider darum.',
-    7 => 'Geeklog-Dateien nicht gefunden',
-    8 => 'Das Installations-Skript hat einige wichtige Geeklog-Dateien nicht gefunden. Wahrscheinlich hast Du diese in andere Verzeichnisse verschoben. Bitte gib hier die Pfade zu den Dateien und Verzeichnissen an:',
-    9 => 'Willkommen und Danke, dass Du Geeklog gewählt hast!',
-    10 => 'Datei/Verzeichnis',
-    11 => 'Zugriffsrechte',
-    12 => 'Ändern auf',
-    13 => 'Derzeit',
+    0 => 'Geeklog - The Ultimate Weblog System',
+    1 => 'Instalacja Pomoc',
+    2 => 'The Ultimate Weblog System',
+    3 => 'Geeklog - Instalacja',
+    4 => 'Wymagane PHP 4.1.0',
+    5 => 'Sorry, ale Geeklog wymaga do uruchomienia co najmniej PHP 4.1.0 (wersja zainstalowana to ',
+    6 => '). Proszê <a href="http://www.php.net/downloads.php">uaktualniæ wersjê PHP</a> lub zg³osiæ to do swojego providera hostingu.',
+    7 => 'Nie mo¿na zlokalizowaæ plików Geeklog-a',
+    8 => 'The installation script was unable to locate critical Geeklog files. This is probably because you have moved them from their default location. Please specify the paths to the files and directories below:',
+    9 => 'Witamy i dziêkujemy za wybranie Geeklog-a!',
+    10 => 'File/Directory',
+    11 => 'Permissions',
+    12 => 'Change to',
+    13 => 'Currently',
     14 => 'Change directory to',
     15 => 'Export of Geeklog headlines is switched off. The <code>backend</code> directory was not tested',
     17 => 'User photos are disabled. The <code>userphotos</code> directory was not tested',
     18 => 'Images in articles are disabled. The <code>articles</code> directory was not tested',
-    19 => 'Geeklog setzt voraus, dass bestimmte Dateien und Verzeichnisse für den Webserver schreibbar sind. Es folgt eine Liste der Dateien und Verzeichnisse, die geändert werden müssen.',
-    20 => 'Warnung!',
-    21 => 'Geeklog und Deine Website werden nicht funktionsfähig sein, solange diese Fehler nicht korrigiert werden. Bitte nimm die notwendigen Änderungen vor, bevor Du mit der Installation fortfährst.',
-    22 => 'unbekannt',
-    23 => 'Bitte wähle eine Installationsmethode:',
-    24 => 'Neuinstallation',
-    25 => 'Upgrade',
+    19 => 'Geeklog requires certain files and directories to be writeable by the web server. Below is a list of which files and directories need to be changed.',
+    20 => 'Ostrze¿enie!',
+    21 => 'Your Geeklog and site will not work properly until the errors listed above are corrected. Not following this step is the #1 reason why people receive errors when they first try to use Geeklog. Please make the necessary changes before you continue.',
+    22 => 'unknown',
+    23 => 'Wybierz rodzaj instalacji:',
+    24 => 'Nowa',
+    25 => 'Aktualizacja',
     26 => 'Unable to modify',
     27 => '. Did you make sure the file is write-able by the web server?',
     28 => 'siteconfig.php. Did you make sure the file is write-able by the web server?',
     29 => 'Geeklog Site',
     30 => 'Another Nifty Geeklog Site',
-    31 => 'Erforderliche Informationen zum Setup',
-    32 => 'Name der Website',
-    33 => 'Site-Slogan',
-    34 => 'Art der Datenbank',
+    31 => 'Wymagane Informacje do Konfiguracji',
+    32 => 'Nazwa Strony',
+    33 => 'Slogan',
+    34 => 'Rodzaj Bazy Danych',
     35 => 'MySQL',
-    36 => 'MySQL mit Support für InnoDB-Tabellen',
+    36 => 'MySQL ze wsparciem Table InnoDB',
     37 => 'Microsoft SQL',
     38 => '',
-    39 => 'Datenbank-Server',
-    40 => 'Name der Datenbank',
-    41 => 'Datenbank-Username',
-    42 => 'Datenbank-Passwort',
-    43 => 'Präfix für Tabellen',
-    44 => 'Optionale Einstellungen',
-    45 => 'URL der Website',
-    46 => '(ohne Slash am Ende)',
-    47 => 'Pfad für das "admin"-Verzeichnis',
-    48 => 'Website-Email-Adresse',
-    49 => '"No Reply"-Email-Adresse',
-    50 => 'Installieren',
-    51 => 'MySQL 3.23.2 benötigt',
+    39 => 'Database Hostname',
+    40 => 'Nazwa Bazy Danych',
+    41 => 'U¿ytkownik Bazy Danych',
+    42 => 'Has³o do Bazy Danych',
+    43 => 'Prefix Tabeli w Bazie Danych',
+    44 => 'Ustawienia Opcjonalne',
+    45 => 'Site URL',
+    46 => '(No trailing slash)',
+    47 => 'Admin Directory Path',
+    48 => 'Site Email',
+    49 => 'Site No-Reply Email',
+    50 => 'Install',
+    51 => 'Wymagana baza MySQL 3.23.2',
     52 => 'Sorry, but Geeklog requires at least MySQL 3.23.2 to run (you have version ',
     53 => '). Please <a href="http://dev.mysql.com/downloads/mysql/">upgrade your MySQL</a> install or ask your hosting service to do it for you.',
-    54 => 'Incorrect database information',
+    54 => 'Ba³êdne informacje o bazie danych',
     55 => 'Sorry, but the database information you entered does not appear to be correct. Please go back and try again.',
     56 => 'Could not connect to database',
     57 => 'Sorry, but the installer could not find the database you specified. Either the database does not exist or you misspelled the name. Please go back and try again.',
     58 => '. Did you make sure the file is write-able by the web server?',
-    59 => 'Hinweis:',
+    59 => 'Notice:',
     60 => 'InnoDB tables are not supported by your version of MySQL. Would you like to continue the installation without InnoDB support?',
-    61 => 'zurück',
-    62 => 'weiter',
+    61 => 'Back',
+    62 => 'Continue',
     63 => 'An installed Geeklog database already exists. The installer will not allow you to run a fresh install on an existing Geeklog database. To continue you must do one of the following:',
     64 => 'Delete the tables from the existing database. Or simply drop the database and recreate it. Then click "Retry" below.',
     65 => 'Perform an upgrade on your database (to a newer Geeklog version) by selecting the "Upgrade" option below.',
-    66 => 'Retry',
+    66 => 'Spróbuj ponownie',
     67 => 'Error Setting up the Geeklog Database', 
     68 => 'The database is not empty. Please drop all tables in the database and start again.',
-    69 => 'Upgrading Geeklog',
+    69 => 'Aktualizacja Geeklog-a',
     70 => 'Before we get started it is important that you back up your database current Geeklog files. This installation script will alter your Geeklog database so if something goes wrong and you need to restart the upgrade process, you will need a backup of your original database. YOU HAVE BEEN WARNED!',
     71 => 'Please make sure to select the correct Geeklog version you are coming from below. This script will do incremental upgrades after this version (i.e. you can upgrade directly from any old version to ',
     72 => ').',
@@ -130,57 +130,57 @@ $LANG_INSTALL = array(
     77 => 'The installer was unable to determine your current version of Geeklog, please select it from the list below:',
     78 => 'Upgrade Error',
     79 => 'An error occured while upgrading your Geeklog installation.',
-    80 => 'Ändern',
+    80 => 'Change',
     81 => 'Stop!',
-    82 => 'Es ist unbedingt nötig, die Zugriffsrechte der unten aufgeführten Dateien zu ändern. Andernfalls wirst Du Geeklog nicht installieren können.',
-    83 => 'Installation Error',
-    84 => 'The path "',
-    85 => '" does not appear to be correct. Please go back and try again.',
-    86 => 'Sprache',
-    87 => 'http://geeklog.info/forum/index.php?forum=1',
+    82 => 'It is critical that you change permissions on the files listed below. Geeklog will not be able to be installed until you do so.',
+    83 => 'B³±d Podczas Instalacji',
+    84 => '¦cie¿ka "',
+    85 => '" jest b³êdna. Wróæ i srpóbuj ponownie.',
+    86 => 'Jêzyk',
+    87 => 'http://www.geeklog.net/forum/index.php?forum=1',
     88 => 'Change directory and containing files to',
-    89 => 'Aktuelle Version:',
-    90 => 'Leere Datenbank?',
+    89 => 'Wersja Obecna:',
+    90 => 'Opró¿niæ bazê danych?',
     91 => 'It appears that either your database is empty or the database credentials you entered are incorrect. Or maybe you wanted to perform a New Install (instead of an Upgrade)? Please go back and try again.',
-    92 => 'Benutze UTF-8'
+    92 => 'U¿yj kodowania UTF-8'
 );
 
 // +---------------------------------------------------------------------------+
 // success.php
 
 $LANG_SUCCESS = array(
-    0 => 'Installation erfolgreich',
-    1 => 'Installation von Geeklog ',
-    2 => ' abgeschlossen!',
-    3 => 'Glückwunsch, Du hast Geeklog erfolgreich ',
-    4 => '. Bitte nimm Dir einen Moment Zeit, um die unten stehenden Informationen zu lesen.',
-    5 => 'Um Dich in Deine neue Geeklog-Site einzuloggen, benutze bitte diesen Account:',
-    6 => 'Username:',
+    0 => 'Instalacja zakoñczona',
+    1 => 'Instalacja Geeklog-a ',
+    2 => ' zakoñczona!',
+    3 => 'Gratulacje, pomy¶lnie ',
+    4 => ' Geeklog-a. Please take a minute to read the information displayed below.',
+    5 => 'Aby zalogowaæ siê proszê u¿yæ nastêpuj±cego konta:',
+    6 => 'U¿ytkownik:',
     7 => 'Admin',
-    8 => 'Passwort:',
+    8 => 'Has³o:',
     9 => 'password',
-    10 => 'Sicherheitshinweis',
-    11 => 'Bitte vergiss nicht, die folgenden ',
-    12 => ' Dinge zu tun',
-    13 => 'Das Installationsverzeichnis löschen oder umbenennen:',
-    14 => 'Das Passwort für den Account ',
-    15 => 'ändern.',
-    16 => 'Die Zugriffsrechte für',
-    17 => 'und',
-    18 => 'zurücksetzen auf',
+    10 => 'Security Warning',
+    11 => 'Don\'t forget to do',
+    12 => 'things',
+    13 => 'Usuñ lub zmieñ nazwê katalogu z plikami instalacyjnymi,',
+    14 => 'Zmieñ has³o dla konta',
+    15 => '.',
+    16 => 'Set permissions on',
+    17 => 'and',
+    18 => 'back to',
     19 => '<strong>Note:</strong> Because the security model has been changed, we have created a new account with the rights you need to administer your new site.  The username for this new account is <b>NewAdmin</b> and the password is <b>password</b>',
-    20 => 'installiert',
-    21 => 'aktualisiert'
+    20 => 'zainstalowano',
+    21 => 'aktualizowano'
 );
 
 // +---------------------------------------------------------------------------+
 // help.php
 
 $LANG_HELP = array(
-    0 => 'Geeklog Installation Support',
-    1 => 'The name of your website.',
-    2 => 'A simple description of your website.',
-    3 => 'Geeklog can be installed using either a MySQL or Microsoft SQL database. If you are not sure which option to select contact your hosting provider.<br><br><strong>Hinweis:</strong> InnoDB-Tabellen können zu besserer Performance auf (sehr) großen Websites führen, machen den Backup-Prozess aber komplizierter.',
+    0 => 'Geeklog Wsparcie Instalacji',
+    1 => 'Nazwa Twojej Strony.',
+    2 => 'Prosty opis Twojej strony.',
+    3 => 'Geeklog can be installed using either a MySQL or Microsoft SQL database. If you are not sure which option to select contact your hosting provider.<br><br><strong>Note:</strong> InnoDB Tables may improve performance on (very) large sites, but they also make database backups more complicated.',
     4 => 'The network name (or IP address) of your database server. This is typically "localhost". If you are not sure contact your hosting provider.',
     5 => 'The name of your database. If you are not sure what this is contact your hosting provider.',
     6 => 'Your database user account. If you are not sure what this is contact your hosting provider.',
