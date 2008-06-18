355a08b28834a1ba1a7ed426dcb7a02920379795
tacahi <devnull@localhost>
2008-06-18 17:37:33 +0000

    geeklog-1.5.0リリース版と異っていたファイルを改めて合わせました。

--- Geeklog-1.x/plugins/polls/language/german_formal_utf-8.php
+++ Geeklog-1.x/plugins/polls/language/german_formal_utf-8.php
@@ -7,7 +7,7 @@
 # addressing the user as "Sie" (formal German).
 #
 # Authors: Dirk Haun <dirk AT haun-online DOT de>
-#          Markus Wollschlﾃ､ger
+#          Markus Wollschläger
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -40,9 +40,9 @@ $LANG_POLLS = array(
     'pollresults' => 'Umfrage-Ergebnisse',
     'votes' => 'Stimmen',
     'vote' => 'Abstimmen',
-    'pastpolls' => 'ﾃ�tere Umfragen',
+    'pastpolls' => 'Ältere Umfragen',
     'savedvotetitle' => 'Stimme gespeichert',
-    'savedvotemsg' => 'Stimme wurde fﾃｼr die Umfrage gespeichert: ',
+    'savedvotemsg' => 'Stimme wurde für die Umfrage gespeichert: ',
     'pollstitle' => 'Umfragen im System',
     'polltopics' => 'Andere Umfragen',
     'stats_top10' => 'Top Ten der Umfragen',
@@ -50,12 +50,12 @@ $LANG_POLLS = array(
     'stats_votes' => 'Stimmen',
     'stats_none' => 'Es gibt keine Umfragen oder es wurden keine Stimmen abgegeben.',
     'stats_summary' => 'Anzahl Umfragen (Stimmen)',
-    'open_poll' => 'Abstimmen mﾃｶglich',
-    'answer_all' => 'Bitte alle ﾃｼbrigen Fragen beantworten',
+    'open_poll' => 'Abstimmen möglich',
+    'answer_all' => 'Bitte alle übrigen Fragen beantworten',
     'not_saved' => 'Ergebnis nicht gespeichert',
     'upgrade1' => 'Neue Version des Umfrage-Plugins installiert. Bitte',
     'upgrade2' => 'upgraden',
-    'editinstructions' => 'Bitte fﾃｼr die Umfrage-ID mindestens eine Frage und zwei Antworten eintragen.'
+    'editinstructions' => 'Bitte für die Umfrage-ID mindestens eine Frage und zwei Antworten eintragen.'
 );
 
 ###############################################################################
@@ -63,7 +63,7 @@ $LANG_POLLS = array(
 
 $LANG25 = array(
     1 => 'Kommentaranzeige',
-    2 => 'Bitte eine Kategorie, mindestens eine Frage und eine Antwort fﾃｼr die Frage eintragen.',
+    2 => 'Bitte eine Kategorie, mindestens eine Frage und eine Antwort für die Frage eintragen.',
     3 => 'Umfrage erstellt',
     4 => 'Umfrage %s gespeichert',
     5 => 'Umfrage editieren',
@@ -77,10 +77,10 @@ $LANG25 = array(
     13 => 'Umfrage erstellen',
     14 => 'speichern',
     15 => 'abbrechen',
-    16 => 'lﾃｶschen',
+    16 => 'löschen',
     17 => 'Bitte Umfrage-ID eingeben',
     18 => 'Liste der Umfragen',
-    19 => 'Um eine Umfrage zu editieren oder zu lﾃｶschen, auf das Edit-Icon klicken.  Um eine neue Umfrage zu erﾃｶffnen, bitte auf "Neu anlegen" oben klicken.',
+    19 => 'Um eine Umfrage zu editieren oder zu löschen, auf das Edit-Icon klicken.  Um eine neue Umfrage zu eröffnen, bitte auf "Neu anlegen" oben klicken.',
     20 => 'Abstimmende',
     21 => 'Kein Zugang',
     22 => "You are trying to access a poll that you don't have rights to.  This attempt has been logged. Please <a href=\"{$_CONF['site_admin_url']}/poll.php\">go back to the poll administration screen</a>.",
@@ -93,19 +93,19 @@ $LANG25 = array(
     29 => 'Suchen',
     30 => 'Ergebnisse eingrenzen',
     31 => 'Frage',
-    32 => 'Um diese Frage aus der Umfrage zu entfernen, den Fragetext lﾃｶschen.',
-    33 => 'Umfrage lﾃ､uft',
+    32 => 'Um diese Frage aus der Umfrage zu entfernen, den Fragetext löschen.',
+    33 => 'Umfrage läuft',
     34 => 'Umfrage-Kategorie:',
     35 => 'Diese Umfrage hat noch ',
     36 => 'Fragen.',
-    37 => 'Ergebnisse ausblenden wenn Umfrage lﾃ､uft',
-    38 => 'Wﾃ､hrend diese Umfrage lﾃ､uft, kﾃｶnnen nur der Eigentﾃｼmer &amp; Root die Ergebnisse sehen.',
-    39 => 'Die Kategorie wird nur angezeigt, wenn sie mehr als eine Frage enthﾃ､lt.',
+    37 => 'Ergebnisse ausblenden wenn Umfrage läuft',
+    38 => 'Während diese Umfrage läuft, können nur der Eigentümer &amp; Root die Ergebnisse sehen.',
+    39 => 'Die Kategorie wird nur angezeigt, wenn sie mehr als eine Frage enthält.',
     40 => 'Alle Antworten zu dieser Umfrage ansehen'
 );
 
 $PLG_polls_MESSAGE19 = 'Umfrage wurde gespeichert.';
-$PLG_polls_MESSAGE20 = 'Umfrage wurde gelﾃｶscht.';
+$PLG_polls_MESSAGE20 = 'Umfrage wurde gelöscht.';
 
 // Messages for the plugin upgrade
 $PLG_polls_MESSAGE3001 = 'Plugin upgrade not supported.';
@@ -118,14 +118,14 @@ $LANG_configsections['polls'] = array(
 );
 
 $LANG_confignames['polls'] = array(
-    'pollsloginrequired' => 'Zur Einsicht einloggen nﾃｶtig?',
-    'hidepollsmenu' => 'Menﾃｼeintrag ausblenden?',
+    'pollsloginrequired' => 'Zur Einsicht einloggen nötig?',
+    'hidepollsmenu' => 'Menüeintrag ausblenden?',
     'maxquestions' => 'Max. Fragen pro Umfrage',
-    'maxanswers' => 'Max. Mﾃｶglichkeiten pro Frage',
+    'maxanswers' => 'Max. Möglichkeiten pro Frage',
     'answerorder' => 'Ergebnisse sortieren ...',
-    'pollcookietime' => 'Voter Cookie gﾃｼltig fﾃｼr',
-    'polladdresstime' => 'Voter IP-Adresse gﾃｼltig fﾃｼr',
-    'delete_polls' => 'Umfragen mit User lﾃｶschen?',
+    'pollcookietime' => 'Voter Cookie gültig für',
+    'polladdresstime' => 'Voter IP-Adresse gültig für',
+    'delete_polls' => 'Umfragen mit User löschen?',
     'aftersave' => 'Nach speichern der Umfrage',
     'default_permissions' => 'Grundeinstellungen Umfragen'
 );
@@ -144,7 +144,7 @@ $LANG_configselects['polls'] = array(
     0 => array('Ja' => 1, 'Nein' => 0),
     1 => array('Ja' => true, 'Nein' => false),
     2 => array('Wie eingereicht' => 'submitorder', 'Nach Abstimmung' => 'voteorder'),
-    9 => array('Zur Umfrage weiterleiten' => 'item', 'Admin Liste anzeigen' => 'list', 'ﾃ貿fentliche Liste anzeigen' => 'plugin', 'Startseite anzeigen' => 'home', 'Kommandozentrale' => 'admin'),
+    9 => array('Zur Umfrage weiterleiten' => 'item', 'Admin Liste anzeigen' => 'list', 'Öffentliche Liste anzeigen' => 'plugin', 'Startseite anzeigen' => 'home', 'Kommandozentrale' => 'admin'),
     12 => array('Kein Zugang' => 0, 'Nur lesen' => 2, 'Lesen-Schreiben' => 3)
 );
 
