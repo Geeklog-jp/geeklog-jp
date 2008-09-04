8138db8db068c14482d98e52d892aa774eb39fad
tacahi <devnull@localhost>
2008-09-04 13:01:28 +0000

    japanizeプラグイン 1.0.0b3 への更新を取り込みます。
    以下のリビジョンの変更を反映させています。
    
    plugins/japanize/functions.inc						r401
    plugins/japanize/sql/sql_japanize_2.php					r401
    plugins/japanize/sql/sql_japanize_3.php					r401

--- plugins/japanize/sql/sql_japanize_3.php
+++ plugins/japanize/sql/sql_japanize_3.php
@@ -5,7 +5,8 @@
 // +---------------------------------------------------------------------------+
 // $Id: sql_japanize3.php
 // もし万一エンコードの種類が  utf-8でない場合は、utf-8に変換してください。
-// 最終更新日　2007/06/29 tsuchi AT geeklog DOT jp
+//@@@@@20080903 「ドリコムRSS」 ウェブサイト変更
+// 最終更新日　2007/09/03 tsuchi AT geeklog DOT jp
 
 //http://www.blogpeople.net/
 $_SQL[] = "
@@ -29,15 +30,20 @@ $_SQL[] = "
     VALUES (NULL, 'ping.bloggers.jp', 'http://ping.bloggers.jp/'
     , 'http://ping.bloggers.jp/rpc/', 'weblogUpdates.ping', 1)
     ";
-//http://ping.rss.drecom.jp/
+//http://ping.rss.drecom.jp/ 
+//@@@@@20080903 ウェブサイト変更
 $_SQL[] = "
     DELETE FROM {$_TABLES['pingservice']} Where
     site_url = 'http://www.myblog.jp/'
     ";
 $_SQL[] = "
+    DELETE FROM {$_TABLES['pingservice']} Where
+    site_url = 'http://rss.drecom.jp/'
+    ";
+$_SQL[] = "
     INSERT INTO {$_TABLES['pingservice']} 
     (pid, name, site_url, ping_url, method, is_enabled)
-    VALUES (NULL, 'ドリコムRSS', 'http://www.myblog.jp/'
+    VALUES (NULL, 'ドリコムRSS', 'http://rss.drecom.jp/'
     , 'http://ping.rss.drecom.jp/', 'weblogUpdates.ping', 1)
     ";
 //http://blog.goo.ne.jp/
