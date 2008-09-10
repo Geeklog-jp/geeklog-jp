d37d8ac690846d04d50ad9d1190803322769a5f4
mystralkk <mystralkk@localhost>
2008-09-10 11:24:51 +0000

    set_var()?????set_file()??????????????????

--- plugins/autotags/admin/index.php
+++ plugins/autotags/admin/index.php
@@ -75,7 +75,7 @@ function form ($A, $error = false)
         $at_template = new Template ($template_path);
 
         $at_template->set_file ('form', 'autotags.thtml');
-        $at_template->set_file ('xhtml', XHTML);
+        $at_template->set_var('xhtml', XHTML);
         $at_template->set_var('layout_url', $_CONF['layout_url']);
         
         $at_template->set_var('site_url', $_CONF['site_url']);
