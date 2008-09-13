9366e5a62f0ba68033ff22d8d69d2b6e2f587a75
tacahi <tacahi@localhost>
2008-09-13 04:58:08 +0000

    geeklog-1.5.1rc1?????????????????????????
    -    $result = $story->SaveSubmission();
    +    $result = $story->saveSubmission();

--- public_html/submit.php
+++ public_html/submit.php
@@ -294,7 +294,7 @@ function savestory ($A)
 
     COM_updateSpeedlimit ('submit');
 
-    $result = $story->SaveSubmission();
+    $result = $story->saveSubmission();
     if( $result == STORY_NO_ACCESS_TOPIC )
     {
         // user doesn't have access to this topic - bail
