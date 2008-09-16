bd3bb1251a4bf86e9add1c8186d069c94586b040
dengenxp <dengenxp@localhost>
2008-09-16 17:04:02 +0000

    r585??????????????????

--- geeklog-1-jp-extended/plugins/calendarjp/sql/mysql_install.php
+++ geeklog-1-jp-extended/plugins/calendarjp/sql/mysql_install.php
@@ -2,12 +2,13 @@
 
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | Calendarjp Plugin 1.0                                                     |
+// | Calendarjp Plugin for Geeklog                                             |
 // +---------------------------------------------------------------------------+
 // | Installation SQL                                                          |
 // +---------------------------------------------------------------------------+
-// | Copyright (C) 2000-2008 by the following authors:                         |
+// | Copyright (C) 2008 by dengen - taharaxp AT gmail DOT com                  |
 // |                                                                           |
+// | Calendarjp plugin is based on prior work by:                              |
 // | Authors: Tony Bibbs        - tony AT tonybibbs DOT com                    |
 // |          Mark Limburg      - mlimburg AT users DOT sourceforge DOT net    |
 // |          Jason Whittenburg - jwhitten AT securitygeeks DOT com            |
