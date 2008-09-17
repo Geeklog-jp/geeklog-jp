b7dd1635523e4407f84e2e45085f05c77ac6babf
ivysoho <ivysoho@localhost>
2008-09-17 08:35:37 +0000

    ??????????????

--- language/japanese_utf-8.php
+++ language/japanese_utf-8.php
@@ -30,7 +30,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ################################################################################
-# Last Update 2008/06/14 by Geeklog.jp group  - info AT geeklog DOT jp
+# Last Update 2008/09/17 by Geeklog.jp group  - info AT geeklog DOT jp
 
 $LANG_CHARSET = 'utf-8';
 
