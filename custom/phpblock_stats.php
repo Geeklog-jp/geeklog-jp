30169d67bfb74237fd3633ceabeb8d05793e35e2
tsuchi000 <devnull@localhost>
2008-09-19 08:42:50 +0000

    phpblock_stats　2006-10-21

--- custom/phpblock_stats.php
+++ custom/phpblock_stats.php
@@ -5,18 +5,18 @@
  * 2006-10-21 Yohichi Yagi.
 **/
 function phpblock_stats() {
-	// グローバル変数
+    // グローバル変数
     global $_CONF, $_TABLES, $LANG10;
 
-	// インクルード
-	require_once($_CONF['path_system'] . 'lib-admin.php');
+    // インクルード
+    require_once($_CONF['path_system'] . 'lib-admin.php');
 
-	// 生成するHTML
-	$disp .= '<div style="text-align:center">';
-	$disp .= COM_NumberFormat(DB_getItem($_TABLES['vars'], 'value', "name = 'totalhits'"));
-	$disp .= '</div>';
-	// HTMLを戻す
-	return $disp;
+    // 生成するHTML
+    $disp .= '<div style="text-align:center">';
+    $disp .= COM_NumberFormat(DB_getItem($_TABLES['vars'], 'value', "name = 'totalhits'"));
+    $disp .= '</div>';
+    // HTMLを戻す
+    return $disp;
 }
 
 ?>
