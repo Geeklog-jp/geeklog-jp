5f84fdb98171c00fa39b5c68e8f774f20308229c
tsuchi000 <tsuchi000@localhost>
2008-09-19 08:42:02 +0000

    phpblock_stats?2006-10-21

---
+++ phpblock_stats.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * スタッツを表示するブロック。
+ * 2006-10-21 Yohichi Yagi.
+**/
+function phpblock_stats() {
+	// グローバル変数
+    global $_CONF, $_TABLES, $LANG10;
+
+	// インクルード
+	require_once($_CONF['path_system'] . 'lib-admin.php');
+
+	// 生成するHTML
+	$disp .= '<div style="text-align:center">';
+	$disp .= COM_NumberFormat(DB_getItem($_TABLES['vars'], 'value', "name = 'totalhits'"));
+	$disp .= '</div>';
+	// HTMLを戻す
+	return $disp;
+}
+
+?>
