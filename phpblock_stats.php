00cc06347164e3402cb030ddd6c4a047ac0e1efc
mystralkk <mystralkk@localhost>
2008-09-20 12:32:56 +0000

    ??????????????

--- phpblock_stats.php
+++ phpblock_stats.php
@@ -1,22 +1,14 @@
 <?php
 
 /**
- * スタッツを表示するブロック。
- * 2006-10-21 Yohichi Yagi.
-**/
+* 訪問者数を表示するブロック。
+* 2006-10-21 Yohichi Yagi.
+*/
 function phpblock_stats() {
-    // グローバル変数
-    global $_CONF, $_TABLES, $LANG10;
-
-    // インクルード
-    require_once($_CONF['path_system'] . 'lib-admin.php');
-
-    // 生成するHTML
-    $disp .= '<div style="text-align:center">';
-    $disp .= COM_NumberFormat(DB_getItem($_TABLES['vars'], 'value', "name = 'totalhits'"));
-    $disp .= '</div>';
-    // HTMLを戻す
+    global $_TABLES;
+	
+    $disp = '<div id="phpblock_stats">'
+		  . COM_NumberFormat(DB_getItem($_TABLES['vars'], 'value', "name = 'totalhits'"))
+		  . '</div>';
     return $disp;
 }
-
-?>
