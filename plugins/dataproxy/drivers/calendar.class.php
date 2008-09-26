623073bf2a912934cbe06c03e41e022e870f6c31
mystralkk <mystralkk@localhost>
2008-09-26 14:21:10 +0000

    ????????????????????????????

--- plugins/dataproxy/drivers/calendar.class.php
+++ plugins/dataproxy/drivers/calendar.class.php
@@ -31,6 +31,10 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 
+if (strpos(strtolower($_SERVER['PHP_SELF']), 'calendar.class.php') !== false) {
+    die('This file can not be used on its own.');
+}
+
 class Dataproxy_calendar extends DataproxyDriver
 {
 	var $driver_name = 'calendar';
