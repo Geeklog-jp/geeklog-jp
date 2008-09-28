88695d7c5eb4f98f5534c420a47061636305c43a
tacahi <devnull@localhost>
2008-09-28 18:37:36 +0000

    Geeklog 1.5.1のマージ時に、変更がうまく反映していなかったprofessinalテーマの ファイルを、改めて1.5.1に合わせた内容にしてコミットします。
    public_html/layout/professional/images/feed.pngは追加が抜けていました。

--- public_html/layout/professional/functions.php
+++ public_html/layout/professional/functions.php
@@ -2,25 +2,24 @@
 
 // this file can't be used on its own
 if (strpos(strtolower($_SERVER['PHP_SELF']), 'functions.php') !== false) {
-    die ('This file can not be used on its own!');
+    die('This file can not be used on its own!');
 }
 
 $_IMAGE_TYPE = 'png';
 
-if (!defined ('XHTML')) {
-    define('XHTML',''); // change this to ' /' for XHTML
+if (!defined('XHTML')) {
+    define('XHTML', ''); // change this to ' /' for XHTML
 }
 
-$result = DB_query ("SELECT onleft,name FROM {$_TABLES['blocks']} WHERE is_enabled = 1");
-$nrows = DB_numRows ($result);
-for ($i = 0; $i < $nrows; $i++) {
-    $A = DB_fetchArray ($result);
-        if ($A['onleft'] == 1) {
-            $_BLOCK_TEMPLATE[$A['name']] = 'blockheader-left.thtml,blockfooter-left.thtml';
-        } else {
-            $_BLOCK_TEMPLATE[$A['name']] = 'blockheader-right.thtml,blockfooter-right.thtml';
-    }
-}
+/*
+ * For left/right block support there is no longer any need for the theme to
+ * put code into functions.php to set specific templates for the left/right
+ * versions of blocks. Instead, Geeklog will automagically look for
+ * blocktemplate-left.thtml and blocktemplate-right.thtml if given
+ * blocktemplate.thtml from $_BLOCK_TEMPLATE. So, if you want different left
+ * and right templates from admin_block, just create blockheader-list-left.thtml
+ * etc.
+ */
 
 $_BLOCK_TEMPLATE['_msg_block'] = 'blockheader-message.thtml,blockfooter-message.thtml';
 $_BLOCK_TEMPLATE['configmanager_block'] = 'blockheader-config.thtml,blockfooter-config.thtml';
@@ -31,10 +30,8 @@ $_BLOCK_TEMPLATE['story_options_block'] = 'blockheader-related.thtml,blockfooter
 // Define the blocks that are a list of links styled as an unordered list - using class="blocklist"
 $_BLOCK_TEMPLATE['admin_block'] = 'blockheader-list.thtml,blockfooter-list.thtml';
 $_BLOCK_TEMPLATE['section_block'] = 'blockheader-list.thtml,blockfooter-list.thtml';
-if (isset($_USER['uid']) && $_USER['uid'] > 1) {
+if (!COM_isAnonUser()) {
     $_BLOCK_TEMPLATE['user_block'] = 'blockheader-list.thtml,blockfooter-list.thtml';
-} else {
-    $_BLOCK_TEMPLATE['user_block'] = 'blockheader.thtml,blockfooter.thtml';
 }
 
 ?>
