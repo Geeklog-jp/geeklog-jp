9ae2fdadee990eec5d2d8d4301d41cb98f834b68
tacahi <devnull@localhost>
2008-09-29 03:14:14 +0000

    さらに、Geeklog 1.5.1のマージに失敗していたファイルを、改めて1.5.1の内容にします。 これらは、いずれも空白の違いだけです。

--- geeklog-1-jp/public_html/switchlang.php
+++ geeklog-1-jp/public_html/switchlang.php
@@ -97,25 +97,25 @@ function switch_language($url, $newlang, $oldlang)
         if ($changed) {
             // merge the pieces back together
             if ($q === false) {
-            $url = implode ('/', $p);
+                $url = implode('/', $p);
             } else {
                 $url = implode('/', $p) . substr($url, $q);
-        }
+            }
         }
 
         $retval = $url;
     } else { // URL contains '?' or '&'
-        $url = explode ('&', $url);
+        $url = explode('&', $url);
         $urlpart = $url[0];
-        if (count ($url) > 1) {
-            array_shift ($url);
-            $extra_vars = '&' . implode ('&', $url);
+        if (count($url) > 1) {
+            array_shift($url);
+            $extra_vars = '&' . implode('&', $url);
         } else {
             $extra_vars = '';
         }
 
-        if (substr ($urlpart, -($lang_len + 1)) == '_' . $oldlang) {
-            $urlpart = substr_replace ($urlpart, $newlang, -$lang_len);
+        if (substr($urlpart, -($lang_len + 1)) == '_' . $oldlang) {
+            $urlpart = substr_replace($urlpart, $newlang, -$lang_len);
         }
 
         $retval = $urlpart . $extra_vars;
@@ -127,8 +127,8 @@ function switch_language($url, $newlang, $oldlang)
 
 // MAIN
 $ret_url = '';
-if (isset ($_SERVER['HTTP_REFERER'])) {
-    if (strpos ($_SERVER['HTTP_REFERER'], $_CONF['site_url']) !== false) {
+if (isset($_SERVER['HTTP_REFERER'])) {
+    if (strpos($_SERVER['HTTP_REFERER'], $_CONF['site_url']) !== false) {
         $ret_url = $_SERVER['HTTP_REFERER'];
     }
 }
@@ -136,44 +136,44 @@ if (isset ($_SERVER['HTTP_REFERER'])) {
 // if not allowed, just ignore and return
 if ($_CONF['allow_user_language'] == 1) {
 
-    COM_setArgNames (array ('lang'));
+    COM_setArgNames(array('lang'));
 
-    $lang = strtolower (COM_applyFilter (COM_getArgument ('lang')));
-    $lang = preg_replace( '/[^a-z0-9\-_]/', '', $lang );
-    $oldlang = COM_getLanguageId ();
+    $lang = strtolower(COM_applyFilter(COM_getArgument('lang')));
+    $lang = preg_replace('/[^a-z0-9\-_]/', '', $lang);
+    $oldlang = COM_getLanguageId();
 
     // do we really have a new language to switch to?
-    if (!empty ($lang) && array_key_exists ($lang, $_CONF['language_files'])) {
+    if (!empty($lang) && array_key_exists($lang, $_CONF['language_files'])) {
 
         // does such a language file exist?
         $langfile = $_CONF['language_files'][$lang];
-        if (is_file ($_CONF['path_language'] . $langfile . '.php')) {
+        if (is_file($_CONF['path_language'] . $langfile . '.php')) {
 
             // Set the language cookie.
             // Mainly used for anonymous users so the rest of their session
             // will remain in the selected language
-            setcookie ($_CONF['cookie_language'], $langfile, time() + 31536000,
-                       $_CONF['cookie_path'], $_CONF['cookiedomain'],
-                       $_CONF['cookiesecure']);
+            setcookie($_CONF['cookie_language'], $langfile, time() + 31536000,
+                      $_CONF['cookie_path'], $_CONF['cookiedomain'],
+                      $_CONF['cookiesecure']);
 
             // if user is not anonymous, store the preference in the database
             if (!COM_isAnonUser()) {
-                DB_query ("UPDATE {$_TABLES['users']} SET language = '{$langfile}' WHERE uid = {$_USER['uid']}");
+                DB_query("UPDATE {$_TABLES['users']} SET language = '{$langfile}' WHERE uid = {$_USER['uid']}");
             }
         }
     }
 
     // Change the language ID if needed
-    if (!empty ($ret_url) && !empty ($lang) && !empty ($oldlang)) {
-        $ret_url = switch_language ($ret_url, $lang, $oldlang);
+    if (!empty($ret_url) && !empty($lang) && !empty($oldlang)) {
+        $ret_url = switch_language($ret_url, $lang, $oldlang);
     }
 }
 
 // if the user didn't come from our site, send them to our index page
-if (empty ($ret_url)) {
+if (empty($ret_url)) {
     $ret_url = $_CONF['site_url'] . '/';
 }
 
-header ("Location: $ret_url");
+header("Location: $ret_url");
 
 ?>
