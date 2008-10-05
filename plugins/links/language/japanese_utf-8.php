2871e2019128087a5e0b0ed5fffa0b6536919c26
ivysoho <devnull@localhost>
2008-10-05 15:06:26 +0000

    r896のコア言語ファイルの変更をgeeklog-1-jp-extendedへ反映させます。

--- plugins/links/language/japanese_utf-8.php
+++ plugins/links/language/japanese_utf-8.php
@@ -12,6 +12,8 @@
 # Tranlated by Geeklog Japanese group
 # Copyright (C) 2008 Takahiro Kambe
 # Additional translation to Japanese by taca AT back-street DOT net
+# Copyright (C) 2006,2007,2008 Geeklog.jp group
+# Additional translation to Japanese by Geeklog.jp group info AT geeklog DOT jp
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -29,7 +31,6 @@
 #
 ###############################################################################
 # $Id: japanese_utf-8.php,v 1.16 2008/06/05 18:48:35 dhaun Exp $
-# Last Update 2008/09/21 by Geeklog.jp group  - info AT geeklog DOT jp
 
 /**
  * This is the english language page for the Geeklog links Plug-in!
