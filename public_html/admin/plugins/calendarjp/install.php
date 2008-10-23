99332f4cc4795fb99e3cc04d284557beaed9b4ae
dengenxp <devnull@localhost>
2008-10-23 16:02:05 +0000

    calendarjpプラグイン：r974の修正をtrunk/geeklog-1-jp-extendedへ反映させます。

--- public_html/admin/plugins/calendarjp/install.php
+++ public_html/admin/plugins/calendarjp/install.php
@@ -43,7 +43,7 @@ require_once '../../../lib-common.php';
 //
 $pi_display_name = 'Calendarjp';
 $pi_name         = 'calendarjp';
-$pi_version      = '1.0.5';
+$pi_version      = '1.0.6';
 $gl_version      = '1.5.0';
 $pi_url          = 'http://www.geeklog.jp/';
 
