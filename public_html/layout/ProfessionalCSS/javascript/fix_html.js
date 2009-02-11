df0526317c056ebf5725a5b6901b2a15bb85050d
tacahi <devnull@localhost>
2009-02-11 13:53:40 +0000

    r1153での変更を branches/geeklog-1.5.1-jp-extended に取り込みます。
    >> r1153:
    >> IE8で、ProfessionalCSSのカラムレイアウト変更ができない問題を修正します。
    >> 同様に、IE8で、WAIproCSSのカラムレイアウト変更ができない問題を修正します。

--- public_html/layout/ProfessionalCSS/javascript/fix_html.js
+++ public_html/layout/ProfessionalCSS/javascript/fix_html.js
@@ -1,6 +1,6 @@
 function FixHTML( leftblocksID, centerblocksID, rightblocksID ) {
   var ua = navigator.userAgent.toLowerCase();
-  var is_ie = ua.indexOf("msie") != -1 && ua.indexOf("opera") == -1;
+  var is_old_ie = (ua.indexOf("msie") != -1) && (ua.indexOf("msie 8") == -1) && (ua.indexOf("opera") == -1);
 
   var leftblocks = document.getElementById(leftblocksID);
   var centerblocks = document.getElementById(centerblocksID);
@@ -20,9 +20,9 @@ function FixHTML( leftblocksID, centerblocksID, rightblocksID ) {
     classValue += ' js_on';
 
     /* HTMLの構造によってbody要素のclass属性に値を設定します。 */
-    if ( is_ie ) {  /* IE用 */
+    if ( is_old_ie ) {  /* IE7以前用 */
       document.body.setAttribute('className', classValue);
-    } else {  /* Gecko, Opera, Safari他用 */
+    } else {  /* Gecko, Opera, Safari, IE8他用 */
       document.body.setAttribute('class', classValue);
     }
 
