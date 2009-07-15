8795ae7e0258df1a2bd4098815eb887c1bbd74b5
tacahi <devnull@localhost>
2009-07-15 14:58:18 +0000

    Geeklog 1.6.0rc2をマージします。
    なお、public_html/fckeditor/editor/filemanager/connectors/php/config.php では、
    以下の変数の設定を *取り敢えず* Geeklog 1.6.0rc2 の方に合わせています。
    
    $Config['QuickUploadPath']['File']
    $Config['QuickUploadAbsolutePath']['File']
    $Config['QuickUploadPath']['Image']
    $Config['QuickUploadAbsolutePath']['Image']
    $Config['QuickUploadPath']['Flash']
    $Config['QuickUploadAbsolutePath']['Flash']
    $Config['QuickUploadPath']['Media']
    $Config['QuickUploadAbsolutePath']['Media']

--- public_html/admin/install/migrate.php
+++ public_html/admin/install/migrate.php
@@ -326,11 +326,13 @@ if (INST_phpOutOfDate()) {
             . '<p><label class="' . $form_label_dir . '">' . $LANG_INSTALL[47] . ' ' . INST_helpLink('site_admin_url') . '</label> <input type="text" name="site_admin_url" value="' . htmlspecialchars($site_admin_url) . '" size="50"' . XHTML . '>  &nbsp; ' . $LANG_INSTALL[46] . '</p>' . LB;
 
         // Identify the backup files in backups/ and order them newest to oldest
-        $sql_files = glob($backup_dir . '*.sql');
-        $tar_files = glob($backup_dir . '*.tar.gz');
-        $tgz_files = glob($backup_dir . '*.tgz');
-        $zip_files = glob($backup_dir . '*.zip');
-        $backup_files = array_merge($sql_files, $tar_files, $tgz_files, $zip_files);
+        $backup_files = array();
+        foreach (array('*.sql', '*.tar.gz', '*.tgz', '*.zip') as $pattern) {
+            $files = glob($backup_dir . $pattern);
+            if (is_array($files)) {
+                $backup_files = array_merge($backup_files, $files);
+            }
+        }
         rsort($backup_files);
 
         $display .= '<p><label class="' . $form_label_dir . '">' . $LANG_MIGRATE[6] . ' ' . INST_helpLink('migrate_file') . '</label>' . LB
@@ -536,7 +538,7 @@ if (INST_phpOutOfDate()) {
                             . '<input type="hidden" name="backup_file" value="' . $backup_file['name'] . '"' . XHTML . '>' . LB
                             . '<input type="hidden" name="language" value="' . $language . '"' . XHTML . '>' . LB
                             . '<input type="submit" class="button big-button" name="overwrite_file" value="' . $LANG_MIGRATE[23] . '"' . XHTML .'>' . LB
-                            . '<input type="button" name="no" value="' . $LANG_MIGRATE[24] . '" onclick="document.location=\'migrate.php\'"' . XHTML .'>' . LB
+                            . '<input type="submit" class="button big-button" name="no" value="' . $LANG_MIGRATE[24] . '" onclick="document.location=\'migrate.php\'"' . XHTML .'>' . LB
                             . '</p></form>' . LB;
 
                     }
@@ -612,6 +614,7 @@ if (INST_phpOutOfDate()) {
                         $num_create++;
                         $line = trim($line);
                         if (strpos($line, 'access') !== false) {
+                            $line = str_replace('IF NOT EXISTS ', '', $line);
                             $words = explode(' ', $line);
                             if (count($words) >= 3) {
                                 $table = str_replace('`', '', $words[2]);
