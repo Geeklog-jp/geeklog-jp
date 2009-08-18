ba987f00334d49f38402b4e182d9efc1fcde56ce
tacahi <devnull@localhost>
2009-08-18 15:08:58 +0000

    trunk/geeklog-1-jpへの変更を戻します。(本ブランチの使用は終了しています。)

--- geeklog-1-jp/plugins/calendarjp/language/english_utf-8.php
+++ geeklog-1-jp/plugins/calendarjp/language/english_utf-8.php
@@ -87,8 +87,7 @@ $LANG_CALJP_1 = array(
     48 => 'Hits',
     49 => 'It appears that there are no events on this site or no one has ever clicked on one.',
     50 => 'Events',
-    51 => 'Delete',
-    'includehttp' => '(include http://)'
+    51 => 'Delete'
 );
 
 $_LANG_CALJP_SEARCH = array(
