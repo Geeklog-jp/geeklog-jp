a84fcb1c8c22a71f4a0730c951ff387f97e2c8fd
tacahi <devnull@localhost>
2009-11-02 03:09:31 +0000

    Geeklog 1.6.1b1をマージします。(take #3).

--- sql/updates/mysql_1.6.0_to_1.6.1.php
+++ sql/updates/mysql_1.6.0_to_1.6.1.php
@@ -3,9 +3,11 @@
 // Add meta tag columns to story table 
 $_SQL[] = "ALTER TABLE {$_TABLES['stories']} ADD meta_description TEXT NULL AFTER frontpage, ADD meta_keywords TEXT NULL AFTER meta_description";
 
-
 // Add meta tag columns to topics table 
 $_SQL[] = "ALTER TABLE {$_TABLES['topics']} ADD meta_description TEXT NULL AFTER imageurl, ADD meta_keywords TEXT NULL AFTER meta_description";
+
+// allow bigger values for topic sort number
+$_SQL[] = "ALTER TABLE {$_TABLES['topics']} CHANGE sortnum sortnum smallint(3) default NULL";
 
 /**
  * Add new config options
@@ -26,6 +28,10 @@ function update_ConfValuesFor161()
 
     // new option to enable / disable closing of comments after x days
     $c->add('article_comment_close_enabled',0,'select',4,21,0, 1685, TRUE);
+
+    // the timezone config option is a dropdown now
+    $utc = addslashes(serialize('UTC')); // change default timezone to UTC
+    DB_query("UPDATE {$_TABLES['conf_values']} SET type = 'select', selectionArray = -1, default_value = '$utc' WHERE name = 'timezone' AND group_name = 'Core'");
 
     return true;
 }
