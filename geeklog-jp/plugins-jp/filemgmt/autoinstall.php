d5367f5da6208fc7c49c2301af36234e95a93dca
dengenxp <devnull@localhost>
2009-12-31 02:37:32 +0000

    filemgmtプラグイン: 新規インストール直後、更新を要求する問題を修正しました。（Issue #132）

--- geeklog-jp/plugins-jp/filemgmt/autoinstall.php
+++ geeklog-jp/plugins-jp/filemgmt/autoinstall.php
@@ -49,7 +49,7 @@ function plugin_autoinstall_filemgmt($pi_name)
     $info = array(
         'pi_name'         => $pi_name,
         'pi_display_name' => $pi_display_name,
-        'pi_version'      => '1.6.0.jp2',
+        'pi_version'      => '1.6.0.jp3',
         'pi_gl_version'   => '1.6.0',
         'pi_homepage'     => 'http://www.geeklog.jp/'
     );
