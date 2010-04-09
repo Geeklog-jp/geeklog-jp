31db9b80d8f4ddc3e1c485ab87d8e608c66df980
tsuchi000 <devnull@localhost>
2010-04-09 05:46:09 +0000

    japanizeプラグイン：1.0.6RC1に更新しました(プラグインPolls2.1.2　polltopics　項目名変更に伴なう修正)

--- plugins-jp/japanize/sql/sql_japanize_6.php
+++ plugins-jp/japanize/sql/sql_japanize_6.php
@@ -4,7 +4,7 @@
 // +---------------------------------------------------------------------------+
 // $Id: sql_japanize6.php
 // もし万一エンコードの種類が  utf-8でない場合は、utf-8に変換してください。
-// 最終更新日　2007/09/01 tsuchi AT geeklog DOT jp
+// 最終更新日　2010/04/09 tsuchi AT geeklog DOT jp GL1.7.0用
 
 
 // 初期日本語サンプルアンケートdelete
@@ -23,89 +23,89 @@ $_SQL[] = "
 
 // 初期日本語サンプルアンケート質問１の答え
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 0, 1, '日本語化をプラグインで実現', 0, '')
     ";
 
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 0, 2, '携帯ハック', 0, '')
     ";
 
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 0, 3, 'すぐに日本語サイトになるインストーラ', 0, '')
     ";
 
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 0, 4, '翻訳されたドキュメント', 0, '')
     ";
 
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 0, 5, '1.4.1からテーマの移行を容易にするProfessionalCSS', 0, '')
     ";
 
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 0, 6, '文字化けしない日本語対応メール関数', 0, '')
     ";
 
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 0, 7, 'その他', 0, '')
     ";
 
 // 初期日本語サンプルアンケート質問２の答え
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 1, 1, '直感的操作', 0, '')
     ";
 
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 1, 2, '権限設定が可能', 0, '')
     ";
 
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 1, 3, 'コミュニティ', 0, '')
     ";
 
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollanswers']} 
+    INSERT INTO {$_TABLES['pollanswers']}
     (`pid`, `qid`, `aid`, `answer`, `votes`, `remark`) VALUES (
     'geeklogfeaturepolljp', 1, 4, '拡張性', 0, '')
     ";
 
 // 初期日本語サンプルアンケート質問
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollquestions']} 
+    INSERT INTO {$_TABLES['pollquestions']}
     (`qid`, `pid`, `question`) VALUES (
     0, 'geeklogfeaturepolljp', '日本語版でお気に入りの機能は何ですか？')
     ";
 
 $_SQL[] = "
-    INSERT INTO {$_TABLES['pollquestions']} 
+    INSERT INTO {$_TABLES['pollquestions']}
     (`qid`, `pid`, `question`) VALUES (
     1, 'geeklogfeaturepolljp', '一番の特徴は何ですか?')
     ";
 
 // 初期日本語サンプルアンケートトピック
 $_SQL[] = "
-    INSERT INTO {$_TABLES['polltopics']} 
-    (`pid`, `topic`, `voters`, `questions`, `date`
+    INSERT INTO {$_TABLES['polltopics']}
+    (`pid`, `topic`, `voters`, `questions`, `created`
     , `display`, `is_open`, `hideresults`, `commentcode`
     , `statuscode`, `owner_id`, `group_id`, `perm_owner`
     , `perm_group`, `perm_members`, `perm_anon`) VALUES (
