dc3a006b3efd3da63e84727cf1c14ac3e3c17948
hirorongl <hirorongl@localhost>
2010-06-20 09:07:56 +0000

    tkgmaps?language??????2.0.4????????

--- plugins-jp/tkgmaps/language/english.php
+++ plugins-jp/tkgmaps/language/english.php
@@ -1,23 +1,11 @@
 <?php
 
-// Reminder: always indent with 4 spaces (no tabs).
 // +---------------------------------------------------------------------------+
-// | Universal Plugin 1.0.3 for Geeklog - The Ultimate Weblog                  |
-// +---------------------------------------------------------------------------|
-// | language/english.php                                                      |
-// +---------------------------------------------------------------------------|
-// | Copyright (C) 2002 by the following authors:                              |
-// |                                                                           |
-// | Author:                                                                   |
-// | Constructed with the Universal Plugin                                     |
-// | Copyright (C) 2002 by the following authors:                              |
-// | Tom Willett                 -    twillett@users.sourceforge.net           |
-// | Blaine Lang                 -    langmail@sympatico.ca                    |
-// | The Universal Plugin is based on prior work by:                           |
-// | Tony Bibbs                  -    tony@tonybibbs.com                       |
-// |                                                                           |
-// | modified by mystral-kk      - geeklog AT mystral-k DOT net                |
-// | modified by dengen          - dengen AT mail DOT trybase DOT com          |
+// | tkgmaps Plugin for Geeklog - The Ultimate Weblog                          |
+// +---------------------------------------------------------------------------+
+// | geeklog/plugins/tkgmaps/language/english.php                              |
+// +---------------------------------------------------------------------------+
+// | Copyright (C) 2009-2010 hiroron - hiroron AT hiroron DOT com              |
 // +---------------------------------------------------------------------------+
 // |                                                                           |
 // | This program is free software; you can redistribute it and/or             |
@@ -35,56 +23,94 @@
 // | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
 // |                                                                           |
 // +---------------------------------------------------------------------------+
-// 2008.05.14 v0.9 customed by G-COE, CSEAS. Addition of GoogleMapsEditor API Auto Tags
-// Authors: Kinoshita
-// Director: IVY WE CO.,LTD. Komma
-// $Id$
 
-// +---------------------------------------------------------------------------+
-// | Array Format:                                                             |
-// | $LANGXX[YY]:   $LANG - variable name                                      |
-// |        XX - file id number                                                |
-// |        YY - phrase id number                                              |
-// +---------------------------------------------------------------------------+
-
-/**
-* Generic Install language
-* 
-* Be sure and change the name of this array to match your plugin
-* e.g. $LANG_ST00
-*
-*/
+if (strpos(strtolower($_SERVER['PHP_SELF']), strtolower(basename(__FILE__))) !== FALSE) {
+    die('This file can not be used on its own!');
+}
 
 $LANG_TKGMAPS = array (
-    'display_name'      => 'GoogleMaps',
-    'menu_title'        => 'GoogleMaps',
-    'plugin'            => 'tkgmaps Plugin',
+    'plugin'            => 'GoogleMaps',
+    'admin'             => 'GoogleMaps Admin',
     'access_denied'     => 'Access Denied',
     'access_denied_msg' => 'Only Root Users have Access to this Page.  Your user name and IP have been recorded.',
-    'admin'             => 'tkgmaps Plugin Admin',
-    'install_header'    => 'Install/Uninstall the tkgmaps Plugin',
-    'installed'         => 'The Plugin is Installed',
-    'uninstalled'       => 'The Plugin is Not Installed',
-    'install_success'   => 'Installation Successful',
-    'install_failed'    => 'Installation Failed -- See your error log to find out why.',
-    'uninstall_msg'     => 'The tkgmaps Plugin Successfully Uninstalled',
-    'install'           => 'Install',
-    'uninstall'         => 'UnInstall',
-    'warning'           => 'Warning!  the tkgmaps Plugin is still Enabled',
-    'enabled'           => 'Disable the plugin before uninstalling.',
-    'readme'            => 'STOP!  Before you press install please read the ',
-    'installdoc'        => 'Install Document.',
-
-    // for stats
-    'stats_headline'    => 'GoogleMaps(Top10)',
-    'stats_title'       => 'Title',
-    'stats_value'       => 'Value',
-    'stats_no_value'    => 'No Data.',
-    
-    // for admin
-    'manager'           => 'GoogleMaps Manager',
-    'instructions'      => 'To modify or delete a data, click on that data\'s edit icon below.  To create a new data, click on "Create New" above.',
-    
 );
+
+$LANG_TKGMAPS_ADMIN = array (
+    'instructions'      => 'Information from the point of creation MG "Points generated from MG" please click.',
+    'mg2point'          => 'Points generated from MG',
+    'notmediagallery'   => 'Valid or is not in MediaGallery. Invalid configuration or in cooperation with MediaGallery.',
+    'mg2point_title'    => 'Points generated from the point of capture information MediaGallery',
+    'mg2point_message'  => 'Please specify a date. MediaGallery images were registered on or after that date will reflect the input Gmap. To reflect all that is left blank and send.',
+    'mg2point_date'     => 'Date',
+    'mg2point_datehelp' => '*)YYYY-DD-MM ex)',
+    'mg2point_submit'   => 'MG run by the uptake',
+    'mg2point_error'    => 'Error!',
+    'mg2point_err_date' => 'is not a valid date.',
+    'mg2point_exec_message' => 'Gmap %s forthe complete capture of location information.',
+    'mg2point_exec_msg_all' => 'All data was up to MediaGallery',
+    'mg2point_exec_msg_date' => 'Data were registered on or after %s days',
+    'mg2point_exec_mg'  => 'The number of MG was extracted from a location',
+    'mg2point_exec_tag' => 'The number of location information that reflects',
+    'mg2point_exec_ins' => 'Add to that the number of actual location',
+    'mg2point_exec_insted' => 'The number of location has been added',
+    'mg2point_exec_count' => '%d',
+    'mgdisp_pointmap_desc' => 'Location of image',
+);
+
+// Localization of the Admin Configuration UI
+$LANG_configsections['tkgmaps'] = array(
+    'label'             => $LANG_TKGMAPS['plugin'],
+    'title'             => $LANG_TKGMAPS['plugin'] . ' Configuration',
+);
+
+$LANG_configsubgroups['tkgmaps'] = array(
+    'sg_main'           => 'Main Settings',
+    'sg_mediagallery'   => 'MediaGallery'
+);
+
+$LANG_fs['tkgmaps'] = array(
+    'fs_main'           => $LANG_TKGMAPS['plugin'] . ' Main Settings',
+    'fs_controls'       => $LANG_TKGMAPS['plugin'] . ' Default controls',
+    'fs_maptypes'       => $LANG_TKGMAPS['plugin'] . ' Default map type of switching',
+    'fs_mediagallery'   => $LANG_TKGMAPS['plugin'] . ' Function with MediaGallery',
+    'fs_mginfo'         => 'MG set a window for capturing information from',
+    'fs_mgtagdisp'      => 'MG autotag in display settings'
+);
+
+$LANG_confignames['tkgmaps'] = array(
+    'gmapapikey'        => 'GoogleMaps API Key',
+    'setmaptype'        => 'The default type map',
+    'infodispevent'     => 'Show info window operation',
+    'infodispcenter'    => 'Show info window on map center',
+    'largectrl'         => 'Large Control',
+    'smallctrl'         => 'Small Control',
+    'smallzoomctrl'     => 'Small Zoom Control',
+    'maptypectrl'       => 'Map Type Control',
+    'overviewctrl'      => 'Over View Control',
+    'scalectrl'         => 'Scale Control',
+    'maptypes'          => 'Select the type of map',
+    'mg2gmaps'          => 'MG to enable collaboration with',
+    'mginfoformat'      => 'Display format of the information window',
+    'mginfoimgformat'   => 'Display format of the image (image)',
+    'mginfoexifformat'  => 'Shooting information display format',
+    'mgdispmgpoint'     => 'mgpoint tag to view the location map',
+    'mgpointmapdesc'    => 'Map Description',
+    'mgpointmapdesc_pos' => 'View Location Map Description',
+    'mgpointmapwidth'   => 'Transverse size of the map(width)',
+    'mgpointmapheight'  => 'Vertical size of the map(height)',
+    'mgpointmapzoom'    => 'Map Zoom(zoom)',
+    'mgpointmaptype'    => 'The default map type Map',
+    'mgdispmgexif'      => 'Show mgexif tag shooting information',
+);
+
+$LANG_configselects['tkgmaps'] = array(
+    0 => array('True' => 1, 'False' => 0),
+    1 => array('Top of the map' => 1, 'Below the map' => 0),
+    2 => array('Click' => 'click', 'Double-click' => 'dblclick', 'Mouse over' => 'mouseover'),
+);
+
+// Messages for the plugin upgrade
+$PLG_tkgmaps_MESSAGE3001 = 'Failed to upgrade the plug-in.';
+$PLG_tkgmaps_MESSAGE3002 = $LANG32[9];
 
 ?>
--- plugins-jp/tkgmaps/language/japanese_utf-8.php
+++ plugins-jp/tkgmaps/language/japanese_utf-8.php
@@ -1,23 +1,11 @@
 <?php
 
-// Reminder: always indent with 4 spaces (no tabs).
 // +---------------------------------------------------------------------------+
-// | Universal Plugin 1.0.3 for Geeklog - The Ultimate Weblog                  |
-// +---------------------------------------------------------------------------|
-// | language/japanese_utf-8.php                                               |
-// +---------------------------------------------------------------------------|
-// | Copyright (C) 2002 by the following authors:                              |
-// |                                                                           |
-// | Author:                                                                   |
-// | Constructed with the Universal Plugin                                     |
-// | Copyright (C) 2002 by the following authors:                              |
-// | Tom Willett                 -    twillett@users.sourceforge.net           |
-// | Blaine Lang                 -    langmail@sympatico.ca                    |
-// | The Universal Plugin is based on prior work by:                           |
-// | Tony Bibbs                  -    tony@tonybibbs.com                       |
-// |                                                                           |
-// | modified by mystral-kk      - geeklog AT mystral-k DOT net                |
-// | modified by dengen          - dengen AT mail DOT trybase DOT com          |
+// | tkgmaps Plugin for Geeklog - The Ultimate Weblog                          |
+// +---------------------------------------------------------------------------+
+// | geeklog/plugins/tkgmaps/language/japanese_utf-8.php                       |
+// +---------------------------------------------------------------------------+
+// | Copyright (C) 2009-2010 hiroron - hiroron AT hiroron DOT com              |
 // +---------------------------------------------------------------------------+
 // |                                                                           |
 // | This program is free software; you can redistribute it and/or             |
@@ -35,57 +23,133 @@
 // | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
 // |                                                                           |
 // +---------------------------------------------------------------------------+
-// 2008.05.14 v0.9 customed by G-COE, CSEAS. Addition of GoogleMapsEditor API Auto Tags
-// Authors: Kinoshita
-// Authors: Hiroron
-// Director: IVY WE CO.,LTD. Komma
-// $Id$
 
-// +---------------------------------------------------------------------------+
-// | Array Format:                                                             |
-// | $LANGXX[YY]:   $LANG - variable name                                      |
-// |        XX - file id number                                                |
-// |        YY - phrase id number                                              |
-// +---------------------------------------------------------------------------+
+if (strpos(strtolower($_SERVER['PHP_SELF']), strtolower(basename(__FILE__))) !== FALSE) {
+    die('This file can not be used on its own!');
+}
 
-/**
-* Generic Install language
-* 
-* Be sure and change the name of this array to match your plugin
-* e.g. $LANG_ST00
-*
-*/
+global $LANG32;
 
 $LANG_TKGMAPS = array (
-    'display_name'      => 'GoogleMaps',
-    'menu_title'        => 'GoogleMaps',
-    'plugin'            => 'GoogleMapsプラグイン',
+    'plugin'            => 'GoogleMaps',
+    'admin'             => 'GoogleMaps管理',
     'access_denied'     => 'アクセスは拒否されました。',
     'access_denied_msg' => 'このページにアクセスできるのは，Rootユーザだけです。あなたのユーザ名とIPアドレスは記録されました。',
-    'admin'             => 'GoogleMapsプラグイン管理',
-    'install_header'    => 'GoogleMapsプラグインのインストール/アンインストール',
-    'installed'         => 'GoogleMapsプラグインはインストールされています。',
-    'uninstalled'       => 'GoogleMapsプラグインはインストールされていません。',
-    'install_success'   => 'GoogleMapsプラグインのインストールに成功しました。',
-    'install_failed'    => 'GoogleMapsプラグインのインストールに失敗しました。詳細はエラーログ(error.log)をご覧ください。',
-    'uninstall_msg'     => 'GoogleMapsプラグインはアンインストールされました。',
-    'install'           => 'インストール',
-    'uninstall'         => 'アンインストール',
-    'warning'           => '警告！　GoogleMapsプラグインは有効なままです。',
-    'enabled'           => 'アンインストールする前に，GoogleMapsプラグインを無効にしてください。',
-    'readme'            => 'ちょっと待って！　「インストール」をクリックする前に，お読みください：',
-    'installdoc'        => 'インストール手順書',
-
-    // for stats
-    'stats_headline'    => 'GoogleMaps(上位10件)',
-    'stats_title'       => '件名',
-    'stats_value'       => '件',
-    'stats_no_value'    => 'データがありません。',
-    'googlemapsapikey' => 'GoogleMaps API Key',
-    // for admin
-    'manager'           => 'GoogleMaps管理',
-    'instructions'      => 'データを修正，削除する場合は各データの「編集」アイコンをクリックしてください。',
-
 );
+
+$LANG_TKGMAPS_ADMIN = array (
+    'instructions'      => 'ファイルからの地点情報作成は「一括登録」を、反対に地点情報からファイルへの出力は「一括書出」をクリックしてください。',
+    'mg2instructions'  => 'MGからの地点情報作成は「MGからポイント生成」をクリックしてください。',
+    'mg2point'          => 'MGからポイント生成',
+    'notmediagallery'   => 'メディアギャラリが入っていないか有効ではありません。もしくはコンフィギュレーションにてメディアギャラリとの連携が無効です。',
+    'mg2point_title'    => 'メディアギャラリから地点情報を取り込みポイント生成',
+    'mg2point_message'  => '日時を指定してください。入力された日時以降に登録されたメディアギャラリの画像をGmapへ反映いたします。空白のまま送信するとすべてを対象にして反映します。',
+    'mg2point_date'     => '日時',
+    'mg2point_datehelp' => '※）YYYY-DD-MM（例）',
+    'mg2point_submit'   => 'MGからの取り込みを実行',
+    'mg2point_error'    => 'エラー。',
+    'mg2point_err_date' => 'は有効な日時ではありません。',
+    'mg2point_exec_message' => '%sを対象にGmapへ位置情報の取り込みが完了しました。',
+    'mg2point_exec_msg_all' => 'メディアギャラリにアップされた全てのデータ',
+    'mg2point_exec_msg_date' => '%s日以降に登録されたデータ',
+    'mg2point_exec_mg'  => 'MGから位置情報が抽出された件数',
+    'mg2point_exec_tag' => '反映対象となる位置情報の件数',
+    'mg2point_exec_ins' => '実際に追加した位置情報の件数',
+    'mg2point_exec_insted' => 'すでに追加済みな位置情報の件数',
+    'mg2point_exec_count' => '%d件',
+    'mgdisp_pointmap_desc' => '画像の場所',
+    'file2point'        => '一括登録',
+    'file2point_title'  => 'ファイルから一括でポイント生成',
+    'file2point_message' => '取り込むファイルを指定して、そのファイルの形式を選択してください。そしてポイント情報を反映するターゲットとIDを入力してください。',
+    'file2point_file'   => 'ファイル',
+    'file2point_filestyle' => 'ファイルの形式',
+    'file2point_target' => '反映先とID',
+    'file2point_submit' => 'ファイルから一括登録を実行',
+    'file2point_error_file' => 'ファイルを指定してください。',
+    'file2point_error_filestyle' => 'ファイル形式を指定してください。',
+    'file2point_error_target' => '反映先を指定してください。',
+    'file2point_error_targetid' => '反映先のIDを指定してください。',
+    'file2point_move_error' => 'アップロードされたファイル %s を移動/コピー中にエラーが発生しました。',
+    'file2point_upload_error' => 'ファイルがアップロードできませんでした。ファイルを保存するディレクトリのパーミッションを確認してください。',
+    'file2point_upload_not_found' => 'アップロードされたファイルが見つかりません。Webサーバーのエラーログをチェックしてください。',
+    'file2point_target_not_found' => '反映先に指定されたIDが存在しません。',
+    'file2point_exec_message' => '%sを対象にGmapへ位置情報の取り込みが完了しました。',
+    'file2point_exec_line'  => 'ファイルの行数',
+    'file2point_exec_fileerr' => 'ファイルから抽出エラー件数',
+    'file2point_exec_total' => '反映対象となる位置情報の件数',
+    'file2point_exec_insted' => '実際に追加した位置情報の件数',
+    'file2point_exec_count' => '%d件',
+    'point2file'        => '一括書出',
+    'point2file_title'  => 'ポイント情報を一括でファイルへ出力',
+    'point2file_message' => '出力したいポイント情報のターゲットとIDを入力してください。そしてファイルの形式を選択してください。',
+    'point2file_target' => 'ターゲットとID',
+    'point2file_filestyle' => 'ファイルの形式',
+    'point2file_filename'  => 'ファイル名',
+    'point2file_submit' => 'ポイント情報からファイルを作成',
+    'point2file_error_target' => 'ターゲットを指定してください。',
+    'point2file_error_targetid' => 'ターゲットのIDを指定してください。',
+    'point2file_error_filestyle' => 'ファイル形式を指定してください。',
+    'point2file_error_filename' => 'ファイル名を指定してください。',
+    'point2file_target_not_found' => 'ターゲットに指定されたIDが存在しません。',
+    'point2file_point_not_found' => 'ターゲットIDにはダウンロードできるデータが存在しません。',
+);
+
+// Localization of the Admin Configuration UI
+$LANG_configsections['tkgmaps'] = array(
+    'label'             => $LANG_TKGMAPS['plugin'],
+    'title'             => $LANG_TKGMAPS['plugin'] . 'の設定',
+);
+
+$LANG_configsubgroups['tkgmaps'] = array(
+    'sg_main'           => '主要設定',
+    'sg_mediagallery'   => 'メディアギャラリ'
+);
+
+$LANG_fs['tkgmaps'] = array(
+    'fs_main'           => $LANG_TKGMAPS['plugin'] . 'の主要設定',
+    'fs_controls'       => $LANG_TKGMAPS['plugin'] . 'のデフォルトコントロール',
+    'fs_maptypes'       => $LANG_TKGMAPS['plugin'] . 'のデフォルトマップ切替の種類',
+    'fs_mediagallery'   => $LANG_TKGMAPS['plugin'] . 'とメディアギャラリの連携機能の設定',
+    'fs_mginfo'         => 'MGから取り込む際の情報ウィンドウの設定',
+    'fs_mgtagdisp'      => 'MGでの自動タグ表示の設定'
+);
+
+$LANG_confignames['tkgmaps'] = array(
+    'gmapapikey'        => 'GoogleMaps API Key',
+    'setmaptype'        => 'デフォルトのマップ種類',
+    'infodispevent'     => '情報ウィンドウを表示する操作',
+    'infodispcenter'    => '情報ウィンドウを地図中央で表示する',
+    'largectrl'         => '大きいコントロール',
+    'smallctrl'         => '小さいコントロール',
+    'smallzoomctrl'     => '小さいズームコントロール',
+    'maptypectrl'       => 'マップ種類切替コントロール',
+    'overviewctrl'      => 'オーバービューコントロール',
+    'scalectrl'         => 'スケールコントロール',
+    'maptypes'          => 'マップ切替の種類',
+    'mg2gmaps'          => 'MGとの連携機能を有効にする',
+    'mg2duplication'    => '重複チェックする',
+    'mginfoformat'      => '情報ウィンドウの表示フォーマット',
+    'mginfoimgformat'   => '画像の表示フォーマット(image)',
+    'mginfokeywordsformat' => 'キーワードの表示フォーマット(keywords)',
+    'mginfoexifformat'  => '撮影情報の表示フォーマット',
+    'mgdispmgpoint'     => 'mgpointタグの位置情報を地図で表示する',
+    'mgpointmapdesc'    => '地図の説明',
+    'mgpointmapdesc_pos' => '地図の説明の表示場所',
+    'mgpointmapwidth'   => '地図の横サイズ(width)',
+    'mgpointmapheight'  => '地図の縦サイズ(height)',
+    'mgpointmapzoom'    => '地図のズーム(zoom)',
+    'mgpointmaptype'    => '地図のデフォルトのマップ種類',
+    'mgdispmgexif'      => 'mgexifタグの撮影情報を表示する',
+);
+
+$LANG_configselects['tkgmaps'] = array(
+    0 => array('はい' => 1, 'いいえ' => 0),
+    1 => array('地図の上' => 1, '地図の下' => 0),
+    2 => array('クリック' => 'click', 'ダブルクリック' => 'dblclick', 'マウスオーバー' => 'mouseover'),
+);
+
+// Messages for the plugin upgrade
+$PLG_tkgmaps_MESSAGE3001 = 'プラグインのアップグレードに失敗しました。';
+$PLG_tkgmaps_MESSAGE3002 = $LANG32[9];
 
 ?>
