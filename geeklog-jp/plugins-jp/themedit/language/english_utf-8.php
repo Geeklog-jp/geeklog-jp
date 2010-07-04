f870a2b477d4816f1b6f29b3772e099c405c25d8
mystralkk <mystralkk@localhost>
2010-07-04 03:03:29 +0000

    ???HTML???XHTML????????????(Issue #153)????????

--- geeklog-jp/plugins-jp/themedit/language/english_utf-8.php
+++ geeklog-jp/plugins-jp/themedit/language/english_utf-8.php
@@ -5,7 +5,7 @@
 // +---------------------------------------------------------------------------|
 // | geeklog/plugins/themedit/language/english_utf-8.php                       |
 // +---------------------------------------------------------------------------|
-// | Copyright (C) 2006-2008 - geeklog AT mystral-kk DOT net                   |
+// | Copyright (C) 2006-2010 - geeklog AT mystral-kk DOT net                   |
 // |                                                                           |
 // | Constructed with the Universal Plugin                                     |
 // | Copyright (C) 2002 by the following authors:                              |
