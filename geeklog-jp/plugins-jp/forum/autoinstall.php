e9db7ed63e3022411da214b4bf99ce1a9f492585
dengenxp <devnull@localhost>
2011-01-16 06:36:29 +0000

    forumプラグイン: オリジナル版のversion 2.7.4をマージしました。これによりBBコード機能に見つかったXSSの脆弱性が取り除かれます。

--- geeklog-jp/plugins-jp/forum/autoinstall.php
+++ geeklog-jp/plugins-jp/forum/autoinstall.php
@@ -62,7 +62,7 @@ function plugin_autoinstall_forum($pi_name)
     $info = array(
         'pi_name'         => $pi_name,
         'pi_display_name' => $pi_display_name,
-        'pi_version'      => '2.7.3.JPr1',
+        'pi_version'      => '2.7.4.JPr1',
         'pi_gl_version'   => '1.6.0',
         'pi_homepage'     => 'http://www.geeklog.jp/'
     );
