61bce0c8a721f49b360f62d3cb798645cbbfa861
mystralkk <devnull@localhost>
2011-06-18 04:42:45 +0000

    本家から公開された自動タグ(autotags)プラグインv1.1.0をマージしました。

--- plugins-jp/autotags/language/english_utf-8.php
+++ plugins-jp/autotags/language/english_utf-8.php
@@ -22,6 +22,7 @@
 #
 ###############################################################################
 
+
 $LANG_AUTO = array(
     'newpage' => 'New Page',
     'adminhome' => 'Admin Home',
@@ -29,21 +30,21 @@ $LANG_AUTO = array(
     'desc' => 'Description',
     'replacement' => 'Replace With',
     'enabled' => 'Enabled?',
-    'function' => 'Replace using function?',
+    'function' => 'Replace with PHP?',
     'short_function' => 'Is Function?',
     'autotagseditor' => 'Autotags Editor',
-    'autotagsmanager' => 'Autotags Manager',
+		'autotagsmanager' => 'Autotags Manager',
     'edit' => 'Edit',
     'save' => 'save',
     'delete' => 'delete',
     'cancel' => 'cancel',
     
     'access_denied' => 'Access Denied',
-    'access_denied_msg' => 'You are illegally trying access one of the Static Pages administration pages.  Please note that all attempts to illegally access this page are logged',
+    'access_denied_msg' => 'You are illegally trying access one of the Autotags administration pages.  Please note that all attempts to illegally access this page are logged',
     'deny_msg' => 'Access to this page is denied.  Either the page has been moved/removed or you do not have sufficient permissions.',
 
-    'php_msg_enabled' => 'If you check the function checkbox, when this tag is encountered the function named with the tag\'s name prefixed with phpautotags_ will be called to translate the tag. The <b>Replace With</b> text will be ignored.',
-    'php_msg_disabled' => 'You must set the $_AUTO_CONF[\'allow_php\'] configuration setting to \'1\' and be in a group with the autotags.PHP feature in order to modify autotags that call a function to translate the tag.',
+    'php_msg_enabled' => 'If you just check the PHP checkbox, when this tag is encountered the function named with the tag\'s name prefixed with phpautotags_ will be called to translate the tag.<br><br>If the PHP checkbox is checked and the <b>Replace With</b> contains text then the text will be evaluated as PHP. To access the first parameter reference the $p1 variable and for the second parameter you would reference $p2 in your PHP script. The variable $tagstr will contain the entire string. Use return in your PHP script to return the text you want to replace the autotag with.',
+    'php_msg_disabled' => 'You must set the Allow PHP configuration setting to true in the Geeklog Configuration panel and be in a group with the autotags.PHP feature in order to modify autotags that call a function to translate the tag.',
     
     'disallowed_tag' => 'The tag you have chosen is restricted and not available for use. Choose another tag.',
     'duplicate_tag' => 'The tag you have chosen is already in use. Please choose another tag name or edit the existing tag.',
@@ -54,24 +55,53 @@ $LANG_AUTO = array(
                         .'<p>Autotags are commonly used to create links. A Replace With field of <b>&lt;a href="http://path.to.somewhere/#1"&gt;#2&lt;/a&gt;</b> when combined with this tag <b>[tag:foo This is a link]</b> will result in the string <b>&lt;a href="http://path.to.somewhere/foo"&gt;This is a link&lt;/a&gt;</b>'
                         .'<p>In addition to #1 and #2, #0 is the entire string after the first colon. #U is the base url of the website.',
 
-    'php_not_activated' => 'The use of PHP in static pages is not activated. Please see the <a href="' . $_CONF['site_url'] . '/docs/english/staticpages.html#php">documentation</a> for details.',
+    'php_not_activated' => 'The use of PHP in Autotags is not activated. Please see the configuration.',
 
     'edit' => 'Edit',
 
     'search' => 'Search',
     'submit' => 'Submit',
     
+    'usagepermissionskey' => 'U = usage',
+    
     'list_all_title' => 'List of Auto Tags',
     'window_close' => 'Close',
-    'main_menulabel' => 'Autotag List',
+    'main_menulabel' => 'Autotag List'
+);
 
-    'descr_story' => 'Links to a story on this website. Title of story is link text: [story:story_id]',
-    'descr_event' => 'Links to an event on this website. Title of the event is link text: [event:event_id]',
-    'descr_calendar' => 'Links to a calendar entry on this website. Title of the entry is link text: [calendar:event_id]',
-    'descr_link' => 'Links to a web resource on this website. Title of remote page is link text: [link:link_id]',
-    'descr_staticpage' => 'Links to a staticpage on this website. Title of the page is link text: [staticpage:page_id]',
-// Added since 1.02jp3
-	'admin_label' => 'Autotags',
+// Localization of the Admin Configuration UI
+$LANG_configsections['autotags'] = array(
+    'label' => 'Autotags',
+    'title' => 'Autotags Configuration'
+);  
+
+$LANG_confignames['autotags'] = array(
+    'link_in_menu' => 'Enable Autotags Menu Entry?',
+    'disallow' => 'Disallowed Autotag Names',
+    'allow_php' => 'Allow PHP',
+    'default_autotag_permissions' => 'Default Permissions'
+);
+
+$LANG_configsubgroups['autotags'] = array(
+    'sg_main' => 'Main Settings'
+);
+
+$LANG_tab['autotags'] = array(
+    'tab_main' => 'General Autotags Settings',
+    'tab_autotag_permissions' => 'Autotag Usage Permissions'
+);
+
+$LANG_fs['autotags'] = array(
+    'fs_main' => 'General Autotags Settings',
+    'fs_autotag_permissions' => 'Autotag Usage Permissions'
+);
+
+// Note: entries 0, 1, and 12 are the same as in $LANG_configselects['Core']
+$LANG_configselects['autotags'] = array(
+    0 => array('True' => 1, 'False' => 0),
+    1 => array('True' => TRUE, 'False' => FALSE),
+    12 => array('No access' => 0, 'Read-Only' => 2, 'Read-Write' => 3),
+    13 => array('No access' => 0, 'Use' => 2)
 );
 
 ?>
