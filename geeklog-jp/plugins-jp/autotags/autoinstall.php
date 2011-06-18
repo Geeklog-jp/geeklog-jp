cedc8a321ee0dceeff7553ff0696cfb67e911669
mystralkk <devnull@localhost>
2011-06-18 04:42:45 +0000

    本家から公開された自動タグ(autotags)プラグインv1.1.0をマージしました。

--- geeklog-jp/plugins-jp/autotags/autoinstall.php
+++ geeklog-jp/plugins-jp/autotags/autoinstall.php
@@ -1,100 +1,132 @@
 <?php
 
-/* Reminder: always indent with 4 spaces (no tabs). */
-// +---------------------------------------------------------------------------+
-// | Autotags Plugin 1.0                                                       |
-// +---------------------------------------------------------------------------+
-// | autoinstall.php                                                           |
-// +---------------------------------------------------------------------------+
-// | Autotags Plugin Copyright (C) 2006 by the following authors:              |
-// |          Joe Mucchiello    - jmucchiello AT yahoo DOT com                 |
-// +---------------------------------------------------------------------------+
-// | Based on the Universal Plugin and prior work by the following authors:    |
-// |                                                                           |
-// | Copyright (C) 2000-2006 by the following authors:                         |
-// |                                                                           |
-// | Authors: Tony Bibbs       - tony AT tonybibbs DOT com                     |
-// |          Tom Willett      - twillett AT users DOT sourceforge DOT net     |
-// |          Blaine Lang      - langmail AT sympatico DOT ca                  |
-// |          Dirk Haun        - dirk AT haun-online DOT de                    |
-// +---------------------------------------------------------------------------+
-// |                                                                           |
-// | This program is free software; you can redistribute it and/or             |
-// | modify it under the terms of the GNU General Public License               |
-// | as published by the Free Software Foundation; either version 2            |
-// | of the License, or (at your option) any later version.                    |
-// |                                                                           |
-// | This program is distributed in the hope that it will be useful,           |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
-// | GNU General Public License for more details.                              |
-// |                                                                           |
-// | You should have received a copy of the GNU General Public License         |
-// | along with this program; if not, write to the Free Software Foundation,   |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
-// |                                                                           |
-// +---------------------------------------------------------------------------+
+/**
+* Autoinstall API functions for the Polls plugin
+*
+* @package Polls
+*/
 
 /**
 * Plugin autoinstall function
 *
 * @param    string  $pi_name    Plugin name
 * @return   array               Plugin information
+*
 */
-function plugin_autoinstall_autotags($pi_name) {
-	$pi_name         = 'autotags';
-	$pi_display_name = 'Autotags';
-	$pi_admin        = $pi_display_name . ' Admin';
+function plugin_autoinstall_autotags($pi_name)
+{
+    $pi_name         = 'autotags';
+    $pi_display_name = 'Autotags';
+    $pi_admin        = $pi_display_name . ' Admin';
 
-	$info = array(
-		'pi_name'         => $pi_name,
-		'pi_display_name' => $pi_display_name,
-		'pi_version'      => '1.02jp3',
-		'pi_gl_version'   => '1.4.0',
-		'pi_homepage'     => 'http://www.geeklog.net/'
-	);
+    $info = array(
+        'pi_name'         => $pi_name,
+        'pi_display_name' => $pi_display_name,
+        'pi_version'      => '1.1.0',
+        'pi_gl_version'   => '1.8.0',
+        'pi_homepage'     => 'http://www.geeklog.net/'
+    );
 
-	$groups = array(
-		$pi_admin => 'Users in this group can administer the '
-					 . $pi_display_name . ' plugin'
-	);
+    $groups = array(
+        $pi_admin => 'Has full access to ' . $pi_display_name . ' features'
+    );
 
-	$features = array(
-		$pi_name . '.admin' => 'Ability to create new Autotags',
-		$pi_name . '.PHP'   => 'Ability to create Autotags which use a PHP function',
-	);
+    $features = array(
+        $pi_name . '.edit'      => 'Access to ' . $pi_name . ' editor',
+        $pi_name . '.PHP'       => 'Ability to create Autotags which use a PHP function', 
+        'config.' . $pi_name . '.tab_main'                  => 'Access to configure general autotag settings',
+        'config.' . $pi_name . '.tab_autotag_permissions'   => 'Access to configure default autotag usage permissions'        
+    );
 
-	$mappings = array(
-		$pi_name . '.admin'     => array($pi_admin)
-	);
+    $mappings = array(
+        $pi_name . '.edit'      => array($pi_admin),
+        'config.' . $pi_name . '.tab_main'                  => array($pi_admin),
+        'config.' . $pi_name . '.tab_autotag_permissions'   => array($pi_admin)        
+    );
 
-# 	$tables = array(
-# 	);
+    $tables = array(
+        'autotags'
+    );
+    
+    $requires = array(
+        array(
+               'db' => 'mysql',
+               'version' => '4.1'
+             )
+    );
+    
 
-	$inst_parms = array(
-		'info'      => $info,
-		'groups'    => $groups,
-		'features'  => $features,
-		'mappings'  => $mappings,
-		'tables'    => $tables
-	);
+    $inst_parms = array(
+        'info'      => $info,
+        'groups'    => $groups,
+        'features'  => $features,
+        'mappings'  => $mappings,
+        'tables'    => $tables,
+        'requires'  => $requires
+    );
 
-	return $inst_parms;
+    return $inst_parms;
+}
+
+/**
+* Load plugin configuration from database
+*
+* @param    string  $pi_name    Plugin name
+* @return   boolean             true on success, otherwise false
+* @see      plugin_initconfig_polls
+*
+*/
+function plugin_load_configuration_autotags($pi_name)
+{
+    global $_CONF;
+
+    $base_path = $_CONF['path'] . 'plugins/' . $pi_name . '/';
+
+    require_once $_CONF['path_system'] . 'classes/config.class.php';
+    require_once $base_path . 'install_defaults.php';
+
+    return plugin_initconfig_autotags();
 }
 
 /**
 * Check if the plugin is compatible with this Geeklog version
 *
 * @param    string  $pi_name    Plugin name
-* @return   boolean             TRUE: plugin compatible; FALSE: not compatible
+* @return   boolean             true: plugin compatible; false: not compatible
+*
 */
-function plugin_compatible_with_this_version_autotags($pi_name) {
-	global $_CONF, $_DB_dbms;
-	
-	return ($_DB_dbms === 'mysql');
+function plugin_compatible_with_this_version_autotags($pi_name)
+{
+    global $_CONF, $_DB_dbms;
+
+    // check if we support the DBMS the site is running on
+    $dbFile = $_CONF['path'] . 'plugins/' . $pi_name . '/sql/'
+            . $_DB_dbms . '_install.php';
+    if (! file_exists($dbFile)) {
+        return false;
+    }
+
+    if (! function_exists('SEC_getGroupDropdown')) {
+        return false;
+    }
+
+    if (! function_exists('SEC_createToken')) {
+        return false;
+    }
+
+    if (! function_exists('COM_showMessageText')) {
+        return false;
+    }
+
+    if (! function_exists('SEC_getTokenExpiryNotice')) {
+        return false;
+    }
+
+    if (! function_exists('SEC_loginRequiredForm')) {
+        return false;
+    }
+
+    return true;
 }
 
-// Database tables
-global $_DB_table_prefix, $_TABLES;
-
-$_TABLES['autotags']  = $_DB_table_prefix . 'autotags_plg';	
+?>
