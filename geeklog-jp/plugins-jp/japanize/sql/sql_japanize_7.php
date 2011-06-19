0c5f7469ff80dd3e6537b19d4de6e869f00d3c7f
mystralkk <devnull@localhost>
2011-06-19 08:22:07 +0000

    Issue #161で不十分だったコードを修正しました。

--- geeklog-jp/plugins-jp/japanize/sql/sql_japanize_7.php
+++ geeklog-jp/plugins-jp/japanize/sql/sql_japanize_7.php
@@ -6,54 +6,39 @@
 // もし万一エンコードの種類が  utf-8でない場合は、utf-8に変換してください。
 // 最終更新日　2008/09/02 tsuchi AT geeklog DOT jp
 
-
 // (18) 初期ブロックタイトル等日本語化
-$_SQL[] = "
-    UPDATE   {$_TABLES['blocks']} SET 
-    title = 'ユーザ情報'
-    WHERE name = 'user_block'
-    ";
-$_SQL[] = "
-    UPDATE   {$_TABLES['blocks']} SET 
-    title = '管理者専用メニュー'
-    WHERE name = 'admin_block'
-    ";
-$_SQL[] = "
-    UPDATE   {$_TABLES['blocks']} SET 
-    title = '記事カテゴリ'
-    WHERE name = 'section_block'
-    ";
-$_SQL[] = "
-    UPDATE   {$_TABLES['blocks']} SET 
-    title = 'アンケート'
-    WHERE name = 'polls_block'
-    ";
-$_SQL[] = "
-    UPDATE   {$_TABLES['blocks']} SET 
-    title = 'イベント'
-    WHERE name = 'events_block'
-    ";
+$_SQL[] = "UPDATE {$_TABLES['blocks']} "
+		. "SET title = 'ユーザー情報' "
+		. "WHERE (name = 'user_block') ";
 
-$_SQL[] = "
-    UPDATE   {$_TABLES['blocks']} SET 
-    title = '新着情報'
-    WHERE name = 'whats_new_block'
-    ";
-$_SQL[] = "
-    UPDATE   {$_TABLES['blocks']} SET 
-    title = 'オンラインユーザ'
-    WHERE name = 'whosonline_block'
-    ";
-$_SQL[] = "
-    UPDATE   {$_TABLES['blocks']} SET 
-    title = '過去記事'
-    WHERE name = 'older_stories'
-    ";
-$_SQL[] = "
-    UPDATE   {$_TABLES['blocks']} SET 
-    title = 'Geeklogについて'
-    ,content = '<p><b>ようこそ、Geeklogへ！</b><p>Geeklogについてのサポートは、 <a href=\"http://www.geeklog.jp\">Geeklog Japanese</a>へ。ドキュメントは <a href=\"http://wiki.geeklog.jp\">Geeklog Wiki ドキュメント</a>をどうぞ。'
-    WHERE name = 'first_block'
-    ";
+$_SQL[] = "UPDATE {$_TABLES['blocks']} "
+		. "SET title = '管理者専用メニュー' "
+		. "WHERE (name = 'admin_block') ";
 
-?>
+$_SQL[] = "UPDATE {$_TABLES['blocks']} "
+		. "SET title = '記事カテゴリ' "
+		. "WHERE (name = 'section_block') ";
+
+$_SQL[] = "UPDATE {$_TABLES['blocks']} "
+		. "SET title = 'アンケート' "
+		. "WHERE (name = 'polls_block') ";
+
+$_SQL[] = "UPDATE {$_TABLES['blocks']} "
+		. "SET title = 'イベント' "
+		. "WHERE (name = 'events_block') ";
+
+$_SQL[] = "UPDATE {$_TABLES['blocks']} "
+		. "SET title = '新着情報' "
+		. "WHERE (name = 'whats_new_block') ";
+
+$_SQL[] = "UPDATE {$_TABLES['blocks']} "
+		. "SET title = 'オンラインユーザー' "
+		. "WHERE (name = 'whosonline_block') ";
+
+$_SQL[] = "UPDATE {$_TABLES['blocks']} "
+		. "SET title = '過去記事' "
+		. "WHERE (name = 'older_stories') ";
+
+$_SQL[] = "UPDATE {$_TABLES['blocks']} "
+		. "SET title = 'Geeklogについて', content = '<p><strong>ようこそ、Geeklogへ!</strong><p>Geeklogについてのサポートは、 <a href=\"http://www.geeklog.jp\">Geeklog Japanese</a>へ。ドキュメントは <a href=\"http://wiki.geeklog.jp\">Geeklog Wiki ドキュメント</a>をどうぞ。' "
+		. "WHERE (name = 'first_block') ";
--- geeklog-jp/plugins-jp/japanize/sql/updates/mysql_1.0.2_to_1.0.3.php
+++ geeklog-jp/plugins-jp/japanize/sql/updates/mysql_1.0.2_to_1.0.3.php
@@ -1,9 +1,5 @@
 <?php
 
-
 //日本語メール用変数追加　 追加
-$_SQL[] = "
-    INSERT INTO {$_TABLES['vars']} VALUES ('japanize_custommail', '0')
-";
-
-?>
+$_SQL[] = "INSERT INTO {$_TABLES['vars']} "
+		. "  VALUES ('japanize_custommail', '0') ";
