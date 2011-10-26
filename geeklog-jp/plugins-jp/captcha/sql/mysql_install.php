ce022485537750d1d32931f90ab59ed6e74d0452
mystralkk <mystralkk@localhost>
2011-10-26 11:57:42 +0000

    MySQL-5.5??????????????"TYPE=MyIsam"???????????????????????

--- geeklog-jp/plugins-jp/captcha/sql/mysql_install.php
+++ geeklog-jp/plugins-jp/captcha/sql/mysql_install.php
@@ -31,7 +31,7 @@ CREATE TABLE {$_TABLES['cp_config']} (
   config_name     varchar(255) NOT NULL default '', 
   config_value    varchar(255) NOT NULL default '', 
   PRIMARY KEY (config_name)
-) TYPE=MyISAM
+) ENGINE=MyISAM
 ";
 
 $_SQL[] = "
@@ -41,7 +41,7 @@ CREATE TABLE {$_TABLES['cp_sessions']} (
   validation      varchar(40) NOT NULL default '', 
   counter         TINYINT(4) NOT NULL default 0, 
   PRIMARY KEY (session_id)
-) TYPE=MyISAM
+) ENGINE=MyISAM
 ";
 
 $_DEFVALUES[] = "INSERT INTO `{$_TABLES['cp_config']}` (`config_name`, `config_value`) VALUES ('anonymous_only', '1'), ('remoteusers','0'), ('debug', '0'), ('enable_comment', '0'), ('enable_contact', '0'), ('enable_emailstory', '0'), ('enable_forum', '0'), ('enable_registration', '0'), ('enable_story', '0'), ('gfxDriver', '3'), ('gfxFormat', 'jpg'), ('gfxPath', '');";
