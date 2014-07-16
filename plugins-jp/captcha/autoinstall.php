263ee842db3e1fecbab303e3f0e9584bfa2d1481
Ivy <devnull@localhost>
2014-07-17 02:08:09 +0900

    Captchaプラグインを3.5にアップデートしました。

--- plugins-jp/captcha/autoinstall.php
+++ plugins-jp/captcha/autoinstall.php
@@ -2,15 +2,15 @@
 
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | CAPTCHA Plugin v4                                                         |
+// | Captcha Plugin 3.5                                                        |
 // +---------------------------------------------------------------------------+
 // | autoinstall.php                                                           |
 // |                                                                           |
 // | This file provides helper functions for the automatic plugin install.     |
 // +---------------------------------------------------------------------------+
-// | Copyright (C) 2009 by the following authors:                              |
+// | Copyright (C) 2013 by the following authors:                              |
 // |                                                                           |
-// | Authors: Hiroron           - hiroron AT hiroron DOT com                   |
+// | Authors: Ben         - ben AT geeklog DOT fr                              |
 // +---------------------------------------------------------------------------+
 // |                                                                           |
 // | This program is free software; you can redistribute it and/or             |
@@ -30,52 +30,64 @@
 // +---------------------------------------------------------------------------+
 
 /**
-* Autoinstall API functions for the CAPTCHA plugin
+* Captcha plugin automatic plugin install
 *
-* @package CAPTCHA
+* @package Captcha
 */
 
-/**
-* Plugin autoinstall function
-*
-* @param    string  $pi_name    Plugin name
-* @return   array               Plugin information
-*
-*/
 function plugin_autoinstall_captcha($pi_name)
 {
     $pi_name         = 'captcha';
-    $pi_display_name = 'CAPTCHA';
+    $pi_display_name = 'Captcha';
     $pi_admin        = $pi_display_name . ' Admin';
 
     $info = array(
         'pi_name'         => $pi_name,
         'pi_display_name' => $pi_display_name,
-        'pi_version'      => '4.0.2',
-        'pi_gl_version'   => '1.4.1',
-        'pi_homepage'     => 'http://hiroron.com/'
+        'pi_version'      => '3.5.5',
+        'pi_gl_version'   => '1.8.0',
+        'pi_homepage'     => 'http://www.geeklog.fr/'
+    );
+
+    $groups = array(
+        $pi_admin => 'Has full access to ' . $pi_display_name . ' features'
+    );
+
+    $features = array(
+        'config.' . $pi_name . '.tab_main'                  => 'Access to configure general captcha settings',
+    );
+
+    $mappings = array(
+        'config.' . $pi_name . '.tab_main'                  => array($pi_admin)
     );
 
     $tables = array(
-        'cp_config',
         'cp_sessions'
     );
 
     $inst_parms = array(
         'info'      => $info,
+        'groups'    => $groups,
+        'features'  => $features,
+        'mappings'  => $mappings,
         'tables'    => $tables
     );
 
     return $inst_parms;
 }
 
-/**
-* Check if the plugin is compatible with this Geeklog version
-*
-* @param    string  $pi_name    Plugin name
-* @return   boolean             true: plugin compatible; false: not compatible
-*
-*/
+function plugin_load_configuration_captcha($pi_name)
+{
+    global $_CONF;
+
+    $base_path = $_CONF['path'] . 'plugins/' . $pi_name . '/';
+
+    require_once $_CONF['path_system'] . 'classes/config.class.php';
+    require_once $base_path . 'install_defaults.php';
+
+    return plugin_initconfig_captcha();
+}
+
 function plugin_compatible_with_this_version_captcha($pi_name)
 {
     global $_CONF, $_DB_dbms;
@@ -87,50 +99,45 @@ function plugin_compatible_with_this_version_captcha($pi_name)
         return false;
     }
 
-    if (! function_exists('SEC_getGroupDropdown')) {
+    if (function_exists('COM_printUpcomingEvents')) {
+        // if this function exists, then someone's trying to install the
+        // plugin on Geeklog 1.4.0 or older - sorry, but that won't work
+        return false;
+    }     
+
+    if (!function_exists('COM_createLink')) {
         return false;
     }
 
-    if (! function_exists('SEC_createToken')) {
+    if (!function_exists('SEC_createToken')) {
         return false;
     }
 
-    if (! function_exists('COM_showMessageText')) {
+    if (!function_exists('COM_showMessageText')) {
         return false;
     }
 
-    if (! function_exists('COM_setLangIdAndAttribute')) {
+    if (!function_exists('SEC_getTokenExpiryNotice')) {
+        return false;
+    }
+
+    if (!function_exists('SEC_loginRequiredForm')) {
         return false;
     }
 
     return true;
 }
 
-/**
-* Plugin postinstall
-*
-* We're inserting our default data here since it depends on other stuff that
-* has to happen first ...
-*
-* @return   boolean     true = proceed with install, false = an error occured
-*
-*/
 function plugin_postinstall_captcha($pi_name)
 {
-    global $_TABLES;
+    global $_TABLES, $_CONF, $_USER;
+	
+    /* This code is for statistics ONLY */
+    $message =  'Completed captcha plugin install: ' .date('m d Y',time()) . "   AT " . date('H:i', time()) . "\n";
+    $message .= 'Site: ' . $_CONF['site_url'] . ' and Sitename: ' . $_CONF['site_name'] . "\n";
+    $pi_version = DB_getItem($_TABLES['plugins'], 'pi_version', "pi_name = 'captcha'");
+    COM_mail("ben@geeklog.fr","$pi_name Version:$pi_version Install successfull",$message);
 
-    $CP_SQL = array();
-    $CP_SQL[] = "INSERT INTO `{$_TABLES['cp_config']}` (`config_name`, `config_value`) VALUES ('anonymous_only', '1'), ('remoteusers','0'), ('debug', '0'), ('enable_comment', '0'), ('enable_contact', '0'), ('enable_emailstory', '0'), ('enable_forum', '0'), ('enable_registration', '0'), ('enable_story', '0'), ('gfxDriver', '3'), ('gfxFormat', 'jpg'), ('gfxPath', '');";
-
-    foreach ($CP_SQL as $sql) {
-        DB_query($sql, 1);
-        if (DB_error()) {
-            COM_error("SQL error in CAPTCHA plugin postinstall, SQL: " . $sql);
-            return false;
-        }
-    }
-
-    return true;
+	return true;
 }
-
 ?>
